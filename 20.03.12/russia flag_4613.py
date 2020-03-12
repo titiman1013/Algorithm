b304f8fb5e17a63e0412f7d5187f7dbd37d1b81c
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-03-12 21:08:23 +0900

    20.03.12 학습

--- 20.03.12/russia flag_4613.py
+++ 20.03.12/russia flag_4613.py
@@ -4,8 +4,18 @@ for tc in range(1, int(input())+1):
     N, M = map(int, input().split())
     arr = [input() for _ in range(N)]
     
+    cnt = 0
     color = 0   # 0이면 흰색 1이면 파란색 2이면 빨간색 칠하기
     for i in range(N):
+        color_cnt = {'W': 0, 'B': 0, 'R': 0}
         for j in range(M):
-            arr[i][j]
+            if arr[i][j] == 'W':
+                color_cnt['W'] += 1
+            elif arr[i][j] == 'B':
+                color_cnt['B'] += 1
+            else:
+                color_cnt['R'] += 1
+        if i == 0:  # 첫줄은 무조건 흰색
+            cnt = M - color_cnt['W']
+        else:
             
