d614c4cd3ad4e52c1f7a3e9744a3a13c17b72623
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-15 02:18:00 +0900

    20.05.15 전기버스 오답

---
+++ 20.05.15/electronic bus_5208.py
@@ -0,0 +1,38 @@
+import sys; sys.stdin = open('electronic bus.txt', 'r')
+
+
+def go(now, gas, cnt):
+    global res
+
+    if cnt >= res:
+        return
+
+    if gas < 0:
+        # print('m')
+        return
+
+    if now == len(arr) - 1:
+        if cnt < res:
+            res = cnt
+            # print(now, gas, cnt)
+            # print(visit)
+        return
+    
+    # visit[now] = True
+    go(now+1, arr[now-1]-1, cnt+1)
+    
+    # visit[now] = False
+    go(now+1, gas-1, cnt)
+
+
+
+for tc in range(1, int(input())+1):
+    arr = list(map(int, input().split()))
+    N = arr.pop(0)
+    arr.append(0)
+
+    # visit = [False] * N
+    res = 10000000
+    go(1, arr[0], 0)
+
+    print(f'#{tc} {res}')
