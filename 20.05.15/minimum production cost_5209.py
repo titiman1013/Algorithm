98495230cf6088df86ae367e7cab453766540181
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-15 12:37:36 +0900

    20.05.15 최소생산비용 해결

--- 20.05.15/minimum production cost_5209.py
+++ 20.05.15/minimum production cost_5209.py
@@ -1,32 +1,27 @@
 import sys; sys.stdin = open('minimum production cost.txt', 'r')
 
-def select(i, j, temp):
+def select(cnt, total):
     global res
+    if res < total:
+        return
 
-    if j == N:
-        select(i+1, 0, temp)
-    
-    else:
-        select(i, j+1, temp+arr[i][j])
+    if sum(success) == N:
+        if total < res:
+            res = total
+        return
 
-
+    for i in range(N):
+        if success[i] == 0:
+            success[i] = 1
+            select(cnt+1, total+arr[cnt][i])
+            success[i] = 0
 
 for tc in range(1, int(input())+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
     success = [0] * N
-    res = 0
-    for i in range(N):
-        temp = 100
-        factory = 0
-        for j in range(N):
-            if success[j] == 1:
-                continue
-            if arr[i][j] < temp:
-                temp = arr[i][j]
-                factory = j
-        success[factory] = 1
-        res += temp
+    res = 1000000000
+    select(0, 0)
 
     print(f'#{tc} {res}')
