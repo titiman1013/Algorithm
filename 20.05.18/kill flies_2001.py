530b73077ed99364e55bb4b52a286b188c4fd8c3
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-18 10:36:06 +0900

    20.05.18 2번문제 해결

--- 20.05.18/kill flies_2001.py
+++ 20.05.18/kill flies_2001.py
@@ -5,8 +5,9 @@ for tc in range(1, int(input())+1):
     arr = [list(map(int, input().split())) for _ in range(N)]
 
     res = 0
-    for i in range(N-M):
-        for j in range(N-M):
+    for i in range(N-M+1):
+        for j in range(N-M+1):
+        # +1 해주지 않으면 가장 끝열을 세지 않음
             temp = 0
             for p in range(M):
                 for q in range(M):
