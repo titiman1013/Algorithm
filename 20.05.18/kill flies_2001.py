17b36ccb81759b7d4df142366f210c51757f6112
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-18 10:35:21 +0900

    20.05.18 2번문제 실패

--- 20.05.18/kill flies_2001.py
+++ 20.05.18/kill flies_2001.py
@@ -4,3 +4,14 @@ for tc in range(1, int(input())+1):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
+    res = 0
+    for i in range(N-M):
+        for j in range(N-M):
+            temp = 0
+            for p in range(M):
+                for q in range(M):
+                    temp += arr[i+p][j+q]
+            if res < temp:
+                res = temp
+    
+    print(f'#{tc} {res}')
