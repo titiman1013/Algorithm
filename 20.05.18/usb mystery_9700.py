40c29264f36489b28132f605eff74f795219fd01
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-18 10:33:33 +0900

    20.05.18 1번문제

---
+++ 20.05.18/usb mystery_9700.py
@@ -0,0 +1,7 @@
+import sys; sys.stdin = open('usb mystery.txt', 'r')
+
+for tc in range(1, int(input())+1):
+    p, q = map(float, input().split())
+    # p: 처음꽂을때 올바른 면으로 꽂을 확률
+    # q: 올바른 면으로 꽂았을 때 정상적으로 꽂힐 확률
+
