083f2f6946341086e4ec42ede7d4922d2a9e7c5f
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-18 10:34:16 +0900

    20.05.18 1번문제 해결

--- 20.05.18/usb mystery_9700.py
+++ 20.05.18/usb mystery_9700.py
@@ -5,3 +5,8 @@ for tc in range(1, int(input())+1):
     # p: 처음꽂을때 올바른 면으로 꽂을 확률
     # q: 올바른 면으로 꽂았을 때 정상적으로 꽂힐 확률
 
+    print(f'#{tc}', end=' ')
+    if (1 - p) * q < p * (1 - q) * q:
+        print('YES')
+    else:
+        print('NO')
