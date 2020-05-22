755b5ed3c5fc364c2d0dc027bb2b715790a95569
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-22 21:37:13 +0900

    20.05.22

---
+++ 20.05.22/minimum tree_5249.py
@@ -0,0 +1,14 @@
+import sys; sys.stdin = open('minimum tree.txt', 'r')
+
+for tc in range(1, int(input())+1):
+    V, E = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(E)]
+    # n1, n2: 양 끝 노드, w: 가중치
+
+for i in range(5):
+    for j in range(5):
+        for k in range(5):
+            pass
+
+for i in range(3):
+    for j in range(5):
