1d10490e3b20ef914c563123d4eead04ab46e49c
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-26 00:16:30 +0900

    20.05.25 9pm 문제2 clear

--- 20.05.25/big_BJ7568.py
+++ 20.05.25/big_BJ7568.py
@@ -1,2 +1,16 @@
 import sys; sys.stdin = open('big.txt', 'r')
 
+arr = []
+for N in range(int(input())):
+    arr.append(list(map(int, input().split()))+[0])
+
+for i in range(len(arr)):
+    n = 0
+    for j in range(len(arr)):
+        if i == j:
+            continue
+        if arr[i][0] < arr[j][0] and arr[i][1] < arr[j][1]:
+            n += 1
+    arr[i][2] = n + 1
+    print(n + 1, end=' ')
+# print()
