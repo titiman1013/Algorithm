13f91796959025cc2ea91827434f6ac1d1ba7ba4
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-26 21:41:57 +0900

    20.05.26 9pm 문제1

--- 20.05.25/remote controler_BJ1107.py
+++ 20.05.25/remote controler_BJ1107.py
@@ -1,2 +1,45 @@
 import sys; sys.stdin = open('remote controler.txt', 'r')
 
+def plus(now):
+    global start, end
+    temp = 0
+    while now != end:
+        now += 1
+        temp += 1
+    
+    return temp
+
+
+def minus(now):
+    global start, end
+    temp = 0
+    while now != end:
+        now -= 1
+        temp += 1
+        if now < 0:
+            temp = 10000000
+            break
+    
+    return temp
+
+
+def press_num(now):
+    global start, end
+    temp = 0
+    now = ''
+    for i in range(len(str(end))):
+        if int(str(end)[i]) not in ban_list:
+            now += str(end)[i]
+        else:
+            pass
+
+
+for tc in range(1, int(input())+1):
+    start = 100
+    end = int(input())
+    N = int(input())
+    ban_list = list(map(int, input().split()))
+    
+    res = min(plus(start), minus(start), press_num(start))
+
+    print(f'#{tc} {res}')
---
+++ 20.05.26/number jump_BJ2210.py
@@ -0,0 +1,2 @@
+import sys; sys.stdin = open('number jump.txt', 'r')
+
