428ec6a73c659d4e99926da4c9dc7b103ba84136
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-05-31 22:00:17 +0900

    20.05.31 문제1 no recursion_fail

---
+++ 20.05.31/looklike russia flag_4613.py
@@ -0,0 +1,58 @@
+import sys; sys.stdin = open('looklike russia flag.txt', 'r')
+
+for tc in range(1, int(input())+1):
+    N, M = map(int, input().split())
+    arr = list(input() for _ in range(N))
+
+    res = 0
+    color = 0
+    # color가 0일땐 white, 1일땐 blue, 2일땐 red
+    for i in range(N):
+        white = 0
+        blue = 0
+        red = 0
+        for j in range(M):
+            if arr[i][j] == 'W':
+                white += 1
+            elif arr[i][j] == 'B':
+                blue += 1
+            else:
+                red += 1
+        
+        if i == 0:
+            res += blue
+            res += red
+        else:
+            if color == 0:
+                if white > blue:
+                    if i + 1 < N - 2:
+                        res += blue
+                        red += red
+                    else:
+                        res += white
+                        res += red
+                        color += 1
+                else:
+                    res += white
+                    res += red
+                    color += 1
+            elif color == 1:
+                if blue > red:
+                    if i + 1 < N - 1:
+                        res += white
+                        res += red
+                    else:
+                        res += white
+                        res += blue
+                        color += 1
+                else:
+                    res += white
+                    res += blue
+                    color += 1
+            elif color == 2:
+                res += white
+                res += blue
+    
+        print(res)
+    
+    print(f'#{tc} {res}')
