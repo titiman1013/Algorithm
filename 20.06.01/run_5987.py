2e12a7a2f64541abd2e8c049b491860e15e89d99
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-06-02 21:48:54 +0900

    20.06.02 문제1

--- 20.06.01/run_5987.py
+++ 20.06.01/run_5987.py
@@ -4,5 +4,3 @@ for tc in range(1, int(input())+1):
     N, M = map(int, input().split())
     # N: 사람수, M: 정보개수
     arr = [list(map(int, input().split())) for _ in range(M)]
-
-    # 포기.. 아이디어 고갈..
