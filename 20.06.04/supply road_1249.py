b286f83a070dacb7cdbf9661e4078ab921d6ec98
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-06-04 15:32:54 +0900

    20.06.04 problem2 bfs1 revise&clear

--- 20.06.04/supply road_1249.py
+++ 20.06.04/supply road_1249.py
@@ -44,43 +44,7 @@ import sys; sys.stdin = open('supply road.txt', 'r')
 #     print(f'#{tc} {res}')
 
 
-# # bfs
-# from collections import deque
-
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 1]
-
-# for tc in range(1, int(input())+1):
-#     N = int(input())
-#     arr = [list(map(int, list(input()))) for _ in range(N)]
-    
-#     for i in range(N):
-#         for j in range(N):
-#             arr[i][j] = [arr[i][j], 0]
-
-#     que = deque()
-#     que.append((0, 0))
-#     while que:
-#         x, y = que.popleft()
-#         for k in range(4):
-#             nx = x + dx[k]
-#             ny = y + dy[k]
-#             if 0 <= nx < N and 0 <= ny < N:
-#                 if arr[nx][ny][1] == 0:
-#                     arr[nx][ny][1] = arr[x][y][1] + arr[nx][ny][0]
-#                     que.append((nx, ny))
-#                 else:
-#                     if arr[x][y][1] + arr[nx][ny][0] < arr[nx][ny][1]:
-#                         arr[nx][ny][1] = arr[x][y][1] + arr[nx][ny][0]
-#                         que.append((nx, ny))
-
-#     # res = min(arr[N-2][N-1][1], arr[N-1][N-2][1])
-#     res = arr[N-1][N-1][1]
-
-#     print(f'#{tc} {res}')
-
-
-# bfs2
+# bfs
 from collections import deque
 
 dx = [-1, 1, 0, 0]
@@ -92,7 +56,7 @@ for tc in range(1, int(input())+1):
     
     for i in range(N):
         for j in range(N):
-            arr[i][j] = [arr[i][j], 10000]
+            arr[i][j] = [arr[i][j], -1]
 
     que = deque()
     que.append((0, 0))
@@ -103,11 +67,48 @@ for tc in range(1, int(input())+1):
             nx = x + dx[k]
             ny = y + dy[k]
             if 0 <= nx < N and 0 <= ny < N:
-                if arr[x][y][1] + arr[nx][ny][0] < arr[nx][ny][1]:
+                if arr[nx][ny][1] == -1:
                     arr[nx][ny][1] = arr[x][y][1] + arr[nx][ny][0]
                     que.append((nx, ny))
+                else:
+                    if arr[x][y][1] + arr[nx][ny][0] < arr[nx][ny][1]:
+                        arr[nx][ny][1] = arr[x][y][1] + arr[nx][ny][0]
+                        que.append((nx, ny))
 
     # res = min(arr[N-2][N-1][1], arr[N-1][N-2][1])
     res = arr[N-1][N-1][1]
 
     print(f'#{tc} {res}')
+
+
+# # bfs2
+# from collections import deque
+
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+# for tc in range(1, int(input())+1):
+#     N = int(input())
+#     arr = [list(map(int, list(input()))) for _ in range(N)]
+    
+#     for i in range(N):
+#         for j in range(N):
+#             arr[i][j] = [arr[i][j], 10000]
+
+#     que = deque()
+#     que.append((0, 0))
+#     arr[0][0][1] = 0
+#     while que:
+#         x, y = que.popleft()
+#         for k in range(4):
+#             nx = x + dx[k]
+#             ny = y + dy[k]
+#             if 0 <= nx < N and 0 <= ny < N:
+#                 if arr[x][y][1] + arr[nx][ny][0] < arr[nx][ny][1]:
+#                     arr[nx][ny][1] = arr[x][y][1] + arr[nx][ny][0]
+#                     que.append((nx, ny))
+
+#     # res = min(arr[N-2][N-1][1], arr[N-1][N-2][1])
+#     res = arr[N-1][N-1][1]
+
+#     print(f'#{tc} {res}')
