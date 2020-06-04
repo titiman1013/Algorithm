c39f3949a6f912b4b4c59ac1c99a5733ab94bcd0
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-06-04 13:34:17 +0900

    20.06.04 problem2 bfs1 time exceed

--- 20.06.04/supply road_1249.py
+++ 20.06.04/supply road_1249.py
@@ -43,3 +43,34 @@ import sys; sys.stdin = open('supply road.txt', 'r')
 
 #     print(f'#{tc} {res}')
 
+
+# bfs
+from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+for tc in range(1, int(input())+1):
+    N = int(input())
+    arr = [list(map(int, list(input()))) for _ in range(N)]
+    
+    for i in range(N):
+        for j in range(N):
+            arr[i][j] = [arr[i][j], 0]
+
+    que = deque()
+    que.append((0, 0))
+    while que:
+        x, y = que.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < N and 0 <= ny < N:
+                if arr[nx][ny][1] == 0:
+                    arr[nx][ny][1] = arr[x][y][1] + arr[nx][ny][0]
+                    que.append((nx, ny))
+                else:
+                    if arr[x][y][1] + arr[nx][ny][0] < arr[nx][ny][1]:
+                        arr[nx][ny][1] = arr[x][y][1] + arr[nx][ny][0]
+
+    print(f'#{tc} {arr[N-1][N-1][1]}')
