6be3573bc630a6a05e4d75485704f9a3d9483996
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-06-10 01:48:46 +0900

    20.06.10 9pm problem1 time exceed

--- 20.06.10/wood cutting_BJ2805.py
+++ 20.06.10/wood cutting_BJ2805.py
@@ -1,2 +1,23 @@
 import sys; sys.stdin = open('wood cutting.txt', 'r')
 
+for tc in range(1, int(input())+1):
+    N, M = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    arr.sort()
+    height = max(arr)
+    temp = 0
+    while True:
+        for i in range(len(arr)-1, -1, -1):
+            if arr[i] > height: 
+                temp += (arr[i] - height)
+            else:
+                break
+        if temp <= M:
+            res = height
+            temp = 0
+            height -= 1
+        else:
+            break
+
+    print(f'#{tc} {res}')
