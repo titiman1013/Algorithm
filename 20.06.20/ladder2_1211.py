39c54297fbd1679ea05409451eac8a5200bc9dec
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-06-20 14:31:42 +0900

    20.06.20 problem1 clear

--- 20.06.20/ladder2_1211.py
+++ 20.06.20/ladder2_1211.py
@@ -1,2 +1,27 @@
 import sys; sys.stdin = open('ladder2.txt', 'r')
 
+for t in range(10):
+    T = int(input())
+    ladders = [list(map(int, input().split())) for i in range(100)]
+ 
+    X = 0
+    MIN = 100000
+    for j in range(100): # 가로로 움직여서 입구찾기
+        if ladders[0][j] == 1:
+            x = j
+            cnt = 0
+ 
+        for i in range(100):
+            if x != 0 and ladders[i][x - 1] == 1:
+                while x != 0 and ladders[i][x - 1] == 1 :
+                    x -= 1
+                    cnt += 1
+            elif x < 99 and ladders[i][x + 1] == 1:
+                while x != 99 and ladders[i][x + 1] == 1:
+                    x += 1
+                    cnt += 1
+         
+        if cnt < MIN:
+            MIN = cnt
+            X = j 
+    print(f'#{t+1} {X}')
