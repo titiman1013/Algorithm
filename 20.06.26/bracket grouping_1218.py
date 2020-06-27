f585060ce1eea1ca4cf11736f108f4571f3d5132
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-06-27 17:33:29 +0900

    20.06.26 problem2 clear

--- 20.06.26/bracket grouping_1218.py
+++ 20.06.26/bracket grouping_1218.py
@@ -1,2 +1,38 @@
 import sys; sys.stdin = open('bracket grouping.txt', 'r')
 
+for tc in range(1, 11):
+    length = int(input())
+    arr = input()
+
+    stack = []
+    res = 0
+    for i in range(length):
+        if arr[i] == '(' or arr[i] == '[' or arr[i] == '{' or arr[i] == '<':
+            stack.append(arr[i])
+        elif arr[i] == ')':
+            if stack[-1] == '(':
+                stack.pop()
+            else:
+                break
+        elif arr[i] == ']':
+            if stack[-1] == '[':
+                stack.pop()
+            else:
+                break
+        elif arr[i] == '}':
+            if stack[-1] == '{':
+                stack.pop()
+            else:
+                break
+        elif arr[i] == '>':
+            if stack[-1] == '<':
+                stack.pop()
+            else:
+                break
+
+    if stack:
+        pass
+    else:
+        res = 1
+
+    print(f'#{tc} {res}')
