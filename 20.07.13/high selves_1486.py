62b6359e6f1b8645147a036cd534947bb120a3d5
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-07-13 23:55:14 +0900

    20.07.13 problem1 clear

--- 20.07.13/high selves_1486.py
+++ 20.07.13/high selves_1486.py
@@ -1,2 +1,30 @@
 import sys; sys.stdin = open('high selves.txt', 'r')
 
+def select(cnt):
+    global res
+    if cnt == N:
+        temp = 0
+        for i in range(N):
+            if check[i]:
+                temp += arr[i]
+        if temp >= B and abs(temp - B) < res:
+            res = abs(temp - B)
+            # print(check)
+        return
+
+    check[cnt] = True
+    select(cnt+1)
+    check[cnt] = False
+    select(cnt+1)
+
+
+
+for tc in range(1, int(input())+1):
+    N, B = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    check = [0] * N
+    res = 100000
+    select(0)
+
+    print(f'#{tc} {res}')
