617cd9b1984b6cac71fcfc995a051b6690991e84
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-07-22 20:36:56 +0900

    20.07.22 problem1 time error

---
+++ 20.07.22/extreme cleaning_4530.py
@@ -0,0 +1,14 @@
+import sys; sys.stdin = open('extreme cleaning.txt', 'r')
+
+# time error
+# for tc in range(1, int(input())+1):
+#     start, end = map(int, input().split())
+
+#     res = 0
+#     for floor in range(start, end):
+#         if floor == 0:
+#             continue
+#         if '4' not in str(floor):
+#             res += 1
+
+#     print(f'#{tc} {res}')
