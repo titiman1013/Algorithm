5e3ed2258e479ad623d2b095b9606ee29d3126dc
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-07-22 23:26:55 +0900

    20.07.22 problem1 solve set

--- 20.07.22/extreme cleaning_4530.py
+++ 20.07.22/extreme cleaning_4530.py
@@ -12,3 +12,20 @@ import sys; sys.stdin = open('extreme cleaning.txt', 'r')
 #             res += 1
 
 #     print(f'#{tc} {res}')
+
+
+#
+for tc in range(1, int(input())+1):
+    start, end = map(int, input().split())
+
+    res = 0
+    for floor in range(start+1, end+1):
+        if floor == 0:
+            continue
+        if '4' not in str(floor):
+            res += 1
+
+    print(f'#{tc} {res}')
+
+
+# 1의자리 10의자리 100의자리 같이 x10마다 다른 for문으로 break를 걸어 풀어보기
