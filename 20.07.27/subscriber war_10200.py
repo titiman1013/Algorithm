366c62fa2f99e16e18a3048e5d046f4b6780c29f
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-07-27 22:51:01 +0900

    20.07.27 problem1 encoding error

--- 20.07.27/subscriber war_10200.py
+++ 20.07.27/subscriber war_10200.py
@@ -1,1 +1,19 @@
 import sys; sys.stdin = open('subscriber war.txt', 'r', encoding="UTF-8")
+
+for tc in range(1, int(input())+1):
+    N, A, B = map(int, input().split())
+
+    max_sub = 0
+    min_sub = 0
+    max_sub = min(A, B)
+    if max(A, B) > N // 2:
+        min_sub = max_sub - (N - max(A, B))
+    else:
+        min_sub = 0
+
+    if min_sub < 0:
+        min_sub = 0
+
+    print(f'#{tc} {max_sub} {min_sub}')
+
+# 인코딩 문제때문에 test case3 이 실행되지 않음
