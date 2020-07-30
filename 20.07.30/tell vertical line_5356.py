f4f451992ef09689e9240a813970691e35144049
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-07-30 23:04:22 +0900

    20.07.30 problem1 clear

--- 20.07.30/tell vertical line_5356.py
+++ 20.07.30/tell vertical line_5356.py
@@ -1,2 +1,19 @@
 import sys; sys.stdin = open('tell vertical line.txt', 'r')
 
+for tc in range(1, int(input())+1):
+    arr = list(input() for _ in range(5))
+
+    max_length = 0
+    for i in range(5):
+        if len(arr[i]) > max_length:
+            max_length = len(arr[i])
+
+    res = ''
+    for i in range(max_length):
+        for j in range(5):
+            if len(arr[j]) > i:
+                res += arr[j][i]
+            else:
+                continue
+
+    print(f'#{tc} {res}')
