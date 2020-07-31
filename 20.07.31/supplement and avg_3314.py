0032db8dfedd68ab2f051f7f99e549e76a629291
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-07-31 23:10:14 +0900

    20.07.31 problem1 clear

--- 20.07.31/supplement and avg_3314.py
+++ 20.07.31/supplement and avg_3314.py
@@ -1,2 +1,13 @@
 import sys; sys.stdin = open('supplement and avg.txt', 'r')
 
+for tc in range(1, int(input())+1):
+    arr = list(map(int, input().split()))
+
+    res = 0
+    for i in range(len(arr)):
+        if arr[i] < 40:
+            res += 40
+        else:
+            res += arr[i]
+
+    print(f'#{tc} {res//5}')
