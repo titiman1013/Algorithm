9b90f5ec786266b9de5ea6b910e8c07728d2a290
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-07-31 23:29:57 +0900

    20.07.31 problem2 time error

--- 20.07.31/under 1million decimal_3131.py
+++ 20.07.31/under 1million decimal_3131.py
@@ -1,1 +1,20 @@
 import sys; sys.stdin = open('under 1million decimal.txt', 'r')
+
+for i in range(1, 1000000, 2):
+    if i == 1:
+        print(2, end=" ")
+        continue
+    
+    str_val = str(i)
+    temp = 0
+    for j in range(len(str_val)):
+        temp += int(str_val[j])
+    if temp == 3:
+        print(3, end=" ")
+        continue
+    if temp % 3:
+        for j in range(2, i):
+            if i != j and i % j == 0:
+                break
+        else:
+            print(i, end=" ")
