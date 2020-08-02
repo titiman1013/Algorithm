7aa721b9f5a0fc20349334d1200810bf2c99b3d7
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-08-02 23:18:56 +0900

    20.08.02 problem1 clear

--- 20.08.02/Base64 Decoder_1928.py
+++ 20.08.02/Base64 Decoder_1928.py
@@ -1,2 +1,12 @@
 import sys; sys.stdin = open('Base64 Decoder.txt', 'r')
 
+from base64 import b64decode
+
+for tc in range(1, int(input())+1):
+    arr = input()
+    print(f'#{tc} {b64decode(arr).decode("UTF-8")}')
+
+
+# for tc in range(1, int(input())+1):
+#     19 6 37 38 25
+#     11 34 31 30
