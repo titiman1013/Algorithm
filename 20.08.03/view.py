2c90de887aff23124afef9c3c50c878e5feda220
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-08-03 18:22:02 +0900

    20.08.03 problem1 clear

--- 20.08.03/view.py
+++ 20.08.03/view.py
@@ -1,1 +1,19 @@
 import sys; sys.stdin = open('view.txt', 'r')
+
+
+for tc in range(1, 11):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    res = 0
+    for i in range(2, N-2):
+        if arr[i] > arr[i-2] and arr[i] > arr[i-1] and arr[i] > arr[i+1] and arr[i] > arr[i+2]:
+            temp = []
+            temp.append(arr[i-2])
+            temp.append(arr[i-1])
+            temp.append(arr[i+1])
+            temp.append(arr[i+2])
+            
+            res += arr[i] - max(temp)
+
+    print(f'#{tc} {res}')
