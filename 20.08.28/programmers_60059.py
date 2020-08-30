93b5c408db2382115b9766c5bf67f37fa0088f63
hyunsukRho <titiman444@gmail.com>
2020-08-30 21:37:21 +0900

    20.08.30 problem1

--- 20.08.28/programmers_60059.py
+++ 20.08.28/programmers_60059.py
@@ -1,3 +1,10 @@
 def solution(key, lock):
     answer = True
+    N = len(lock)
+    M = len(key)
+    temp_lock = 
     return answer
+
+
+
+print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
---
+++ 20.08.30/code creater_1225.py
@@ -0,0 +1,2 @@
+import sys; sys.stdin = open('code creater.txt', 'r')
+
