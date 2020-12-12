778c9f133361554d4fc8fe180eaa084d965f6ac0
hyunsukRho <hyunsukr1013@gmail.com>
2020-12-12 22:10:24 +0900

    20.12.12 problem1 solve2 fail

--- 20.12.12/백준_20057.py
+++ 20.12.12/백준_20057.py
@@ -1,5 +1,6 @@
 import sys; sys.stdin = open('text1.txt', 'r')
 
+# solve test1 fail
 # dx = [-2, -1, -1, -1, 0, 1, 1, 1, 2]
 # dy = [0, -1, 0, 1, -2, -1, 0, 1, 0]
 # per = [2, 10, 7, 1, 5, 10, 7, 1, 2]
@@ -66,3 +67,114 @@ import sys; sys.stdin = open('text1.txt', 'r')
 #     print(f'#{tc} {res}')
 
 
+# solve test2
+dx = [-2, -1, -1, -1, 0, 1, 1, 1, 2]
+dy = [-1, -2, -1, 0, -3, -2, -1, 0, -1]
+per = [2, 10, 7, 1, 5, 10, 7, 1, 2]
+
+for tc in range(1, int(input()) + 1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    # 1: 좌, 2: 하, 3: 우, 4: 상
+    direction = 1
+    x = N // 2
+    y = N // 2
+    res = 0
+    move_list = []
+    for i in range(N):
+        if i != N - 1 and i != 0:
+            for _ in range(2):
+                move_list.append(i)
+        elif i == N - 1:
+            for _ in range(3):
+                move_list.append(i)
+
+    for move in move_list:
+        for _ in range(move):
+            if direction == 1:
+                if y - 1 < 0:
+                    res += arr[x][y]
+                else:
+                    sand = arr[x][y - 1]
+                    blowed_sand = 0
+                    for i in range(9):
+                        nx = x + dx[i]
+                        ny = y + dy[i] - 1
+                        # temp_sand = int(arr[x][y] * (per[i] / 100))
+                        temp_sand = (sand * per[i]) // 100
+                        if 0 <= nx < N and 0 <= ny < N:
+                            arr[nx][ny] += temp_sand
+                        else:
+                            res += temp_sand
+                        blowed_sand += temp_sand
+                    arr[x][y - 1] = 0
+                    y -= 1
+                    arr[x][y - 1] += (sand - blowed_sand)
+                    
+            elif direction == 2:
+                if x + 1 >= N:
+                    res += arr[x][y]
+                else:
+                    sand = arr[x + 1][y]
+                    blowed_sand = 0
+                    for i in range(9):
+                        nx = x - dy[i] + 1
+                        ny = y + dx[i]
+                        # temp_sand = int(arr[x][y] * (per[i] / 100))
+                        temp_sand = (sand * per[i]) // 100
+                        if 0 <= nx < N and 0 <= ny < N:
+                            arr[nx][ny] += temp_sand
+                        else:
+                            res += temp_sand
+                        blowed_sand += temp_sand
+                    arr[x + 1][y] = 0
+                    x += 1
+                    arr[x + 1][y] += (sand - blowed_sand)
+
+            elif direction == 3:
+                if y + 1 >= N:
+                    res += arr[x][y]
+                else:
+                    sand = arr[x][y + 1]
+                    blowed_sand = 0
+                    for i in range(9):
+                        nx = x - dx[i]
+                        ny = y - dy[i] + 1
+                        # temp_sand = int(arr[x][y] * (per[i] / 100))
+                        temp_sand = (sand * per[i]) // 100
+                        if 0 <= nx < N and 0 <= ny < N:
+                            arr[nx][ny] += temp_sand
+                        else:
+                            res += temp_sand
+                        blowed_sand += temp_sand
+                    arr[x][y + 1] = 0
+                    y += 1
+                    arr[x][y + 1] += (sand - blowed_sand)
+
+            elif direction == 4:
+                if x - 1 < 0:
+                    res += arr[x][y]
+                else:
+                    sand = arr[x - 1][y]
+                    blowed_sand = 0
+                    for i in range(9):
+                        nx = x - dy[i] - 1
+                        ny = y - dx[i]
+                        # temp_sand = int(arr[x][y] * (per[i] / 100))
+                        temp_sand = (sand * per[i]) // 100
+                        if 0 <= nx < N and 0 <= ny < N:
+                            arr[nx][ny] += temp_sand
+                        else:
+                            res += temp_sand
+                        blowed_sand += temp_sand
+                    arr[x - 1][y] = 0
+                    x -= 1
+                    arr[x - 1][y] += (sand - blowed_sand)
+
+        direction += 1
+        if direction == 5:
+            direction = 1
+    
+    print(f'#{tc} {res}')
+    print(arr)
