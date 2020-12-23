499f964b32a177293620d9a799ff708993d5ce2b
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-12-23 23:02:24 +0900

    20.12.23 problem1 bfs clear

--- 20.12.23/BOJ_1697.py
+++ 20.12.23/BOJ_1697.py
@@ -24,3 +24,33 @@ import sys; sys.stdin = open('text1.txt', 'r')
 # min_sec = 100000000
 # res = check(N, 0)
 # print(min_sec)
+
+# bfs
+from collections import deque
+
+
+def find(N):
+    global K
+
+    q = deque()
+    q.append(N)
+    
+    while q:
+        x = q.popleft()
+
+        if x == K:
+            return arr[x]
+
+        for i in [x + 1, x - 1, x * 2]:
+            if 0 <= i < 100001 and arr[i] == 0:
+                arr[i] = arr[x] + 1
+                q.append(i)
+
+
+
+N, K = map(int, input().split())
+arr = [0] * 100001
+# 예외 존재
+# arr[0] = 1
+res = find(N)
+print(res)
