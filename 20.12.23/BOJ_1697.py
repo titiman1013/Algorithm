a2fc31d542b7a5ca4fda78bb1d285e4a440d6df9
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-12-23 22:51:47 +0900

    20.12.23 problem1 reculsive false

--- 20.12.23/BOJ_1697.py
+++ 20.12.23/BOJ_1697.py
@@ -1,3 +1,26 @@
 import sys; sys.stdin = open('text1.txt', 'r')
 
-N, K = map(int, input().split())
+# reculsive fail
+
+# def check(pos, sec):
+#     global min_sec, K
+
+#     if pos == K:
+#         if sec < min_sec:
+#             min_sec = sec
+#         return
+
+#     if pos > K:
+#         check(pos - 1, sec + 1)
+
+#     else:
+#         check(pos + 1, sec + 1)
+#         check(pos * 2, sec + 1)
+
+
+
+# N, K = map(int, input().split())
+
+# min_sec = 100000000
+# res = check(N, 0)
+# print(min_sec)
