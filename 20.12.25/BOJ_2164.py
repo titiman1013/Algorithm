4069c5061a3051081f6554a6640af94f6749e0e9
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-12-25 11:19:37 +0900

    20.12.25 problem1 clear

--- 20.12.25/BOJ_2164.py
+++ 20.12.25/BOJ_2164.py
@@ -1,3 +1,23 @@
-import sys; sys.stdin = eopn('text1.txt', 'r')
+import sys; sys.stdin = open('text1.txt', 'r')
+from collections import deque
+
+
+def create(N):
+    q = deque()
+    for i in range(N):
+        q.append(i)
+    return q
+
+
+def shuffle():
+    while len(arr) != 1:
+        x = arr.popleft()
+        temp = arr.popleft()
+        arr.append(temp)
+    return arr[0] + 1
+
 
 N = int(input())
+arr = create(N)
+res = shuffle()
+print(res)
