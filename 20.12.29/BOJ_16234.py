26796fac0216e174cce75df980a8cb6f8fa9413f
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-12-29 19:22:24 +0900

    20.12.29 problem1

---
+++ 20.12.29/BOJ_16234.py
@@ -0,0 +1,6 @@
+import sys; sys.stdin = open('text1.txt', 'r')
+
+for tc in range(1, int(input()) + 1):
+    N, L, R = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    
