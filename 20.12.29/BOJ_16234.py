17e5ef4dbbc83b0594adecdfdd7471d741c93a18
hyunsuk Rho <hyunsukr1013@gmail.com>
2020-12-29 23:40:40 +0900

    20.12.29 problem1 false

--- 20.12.29/BOJ_16234.py
+++ 20.12.29/BOJ_16234.py
@@ -1,4 +1,5 @@
 import sys; sys.stdin = open('text1.txt', 'r')
+from collections import deque
 
 
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
@@ -8,21 +9,28 @@ def check():
     global res
 
     cnt = 0
-    for x in range(N):
-        for y in range(N):
+    for i in range(N):
+        for j in range(N):
             merge_cnt = 0
             merge_lst = []
-            total_people = arr[x][y]
-            for k in range(4):
-                nx, ny = x + dx[k], y + dy[k]
-                if 0 <= nx < N and 0 <= ny < N:
-                    if L <= abs(arr[x][y] - arr[nx][ny]) <= R:
-                        merge_lst.append((nx, ny))
-                        total_people += arr[nx][ny]
-                        merge_cnt += 1
+            total_people = arr[i][j]
+            q = deque()
+            q.append((i, j))
+
+            while q:
+                x, y = q.popleft()
+                for k in range(4):
+                    nx, ny = x + dx[k], y + dy[k]
+                    if 0 <= nx < N and 0 <= ny < N:
+                        if L <= abs(arr[x][y] - arr[nx][ny]) <= R and (nx, ny) not in merge_lst:
+                            merge_lst.append((nx, ny))
+                            q.append((nx, ny))
+                            total_people += arr[nx][ny]
+                            merge_cnt += 1
+
             if merge_cnt:
-                for i, j in merge_lst:
-                    arr[i][j] = total_people // (merge_cnt + 1)
+                for a, b in merge_lst:
+                    arr[a][b] = total_people // (merge_cnt + 1)
                 arr[x][y] = total_people // (merge_cnt + 1)
                 cnt += 1
     if cnt:
