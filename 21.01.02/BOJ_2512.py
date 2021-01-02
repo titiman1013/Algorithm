e94005ac74708e6899cd59b66c97e404a67e4b50
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-02 23:07:36 +0900

    21.01.02 problem1 false

--- 21.01.02/BOJ_2512.py
+++ 21.01.02/BOJ_2512.py
@@ -1,5 +1,34 @@
 import sys; sys.stdin = open('text1.txt', 'r')
 
+def check(temp_arr):
+    temp = 0
+    for i in range(N):
+        temp += temp_arr[i]
+    if temp <= M:
+        return temp
+
+    avg = temp // N
+    temp_arr.sort()
+    temp_lst = []
+    upper_lst = []
+    for i in range(N):
+        if temp_arr[i] <= avg:
+            temp_lst.append(temp_arr[i])
+        else:
+            upper_lst.append(temp_arr[i])
+    
+    remain_val = M - sum(temp_lst)
+    remain_avg = remain_val // len(upper_lst)
+    for i in range(len(upper_lst)):
+        temp_lst.append(remain_avg)
+    print(temp_lst)
+    return max(temp_lst)
+
+
+
 N = int(input())
 arr = list(map(int, input().split()))
 M = int(input())
+
+res = check(arr)
+print(res)
