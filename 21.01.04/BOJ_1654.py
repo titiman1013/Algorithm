d1637a882580b8c325e84dc0562e7299af287a9d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-05 00:10:15 +0900

    21.01.04 problem1 runtime error

--- 21.01.04/BOJ_1654.py
+++ 21.01.04/BOJ_1654.py
@@ -1,4 +1,28 @@
 import sys; sys.stdin = open('text1.txt', 'r')
+from collections import Counter
+
+def binary_search(temp_arr):
+    # start, end
+    s, e = 0, max(temp_arr)
+
+    while s <= e:
+        temp = 0
+        mid = (s + e) // 2
+        for val, cnt in temp_arr.items():
+            if val // mid:
+                temp += (val // mid) * cnt
+        
+        if temp >= N:
+            res = mid
+            s = mid + 1
+        else:
+            e = mid - 1
+    
+    return res
+
 
 K, N = map(int, input().split())
-arr = [int(input()) for _ in range(K)]
+arr = Counter(int(input()) for _ in range(K))
+
+res = binary_search(arr)
+print(res)
