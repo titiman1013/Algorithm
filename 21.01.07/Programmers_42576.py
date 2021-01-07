8439cedaa97309d98d432b92734d01ac7a634611
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-07 10:38:42 +0900

    21.01.07 problem1 clear

--- 21.01.07/Programmers_42576.py
+++ 21.01.07/Programmers_42576.py
@@ -1,14 +1,27 @@
+from collections import Counter
+
+
 def solution(participant, completion):
-    answer = ''
-    return answer
+    participant_lst = Counter(participant)
+    completion_lst = Counter(completion)
 
+    if len(participant_lst) != len(completion_lst):
+        for key in participant_lst.keys():
+            if key not in completion_lst:
+                return key
+    else:
+        for key, val in participant_lst.items():
+            if completion_lst[key] != val:
+                return key
 
+    # answer = ''
+    # return answer
 
 
 
 print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
 print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
-print(solution([["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]]))
+print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
 
 # answer
 # "leo"
