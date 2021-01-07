bfead5c830d66540560dec04f32d6145440896d9
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-07 11:22:21 +0900

    21.01.07 problem2

---
+++ 21.01.07/Programmers_42577.py
@@ -0,0 +1,15 @@
+def solution(phone_book):
+    answer = True
+    return answer
+
+
+
+
+print(solution(["119", "97674223", "1195524421"]))
+print(solution(["123","456","789"]))
+print(solution(["12","123","1235","567","88"]))
+
+# answer
+# false
+# true
+# false
