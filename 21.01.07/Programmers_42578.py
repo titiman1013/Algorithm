d79f6fb251e719520656e82bea60ce33cb814d5d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-07 16:23:05 +0900

    21.01.07 problem3 clear

--- 21.01.07/Programmers_42578.py
+++ 21.01.07/Programmers_42578.py
@@ -1,7 +1,36 @@
-def solution(clothes):
-    answer = 0
-    return answer
+# first solve
 
+# def solution(clothes):
+#     hash_map = {}
+#     for cloth in clothes:
+#         if cloth[1] in hash_map:
+#             hash_map[cloth[1]] += 1
+#         else:
+#             hash_map[cloth[1]] = 1
+    
+#     answer = 0
+#     for val in hash_map.values():
+#         if answer:
+#             answer *= (val + 1)
+#         else:
+#             answer = val + 1
+    
+#     return answer - 1
+    
+    
+    
+    # answer = 0
+    # return answer
+
+
+
+# best solve
+
+from collections import Counter
+from functools import reduce
+
+def solution(clothes):
+    return reduce(lambda x, y: x * y, [val + 1 for val in Counter([cloth[1] for cloth in clothes]).values()]) - 1
 
 
 
