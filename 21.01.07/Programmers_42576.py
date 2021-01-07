52adb13de5d088f5933bd7ca1464f2efbd916144
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-07 10:23:40 +0900

    21.01.07 problem1

---
+++ 21.01.07/Programmers_42576.py
@@ -0,0 +1,16 @@
+def solution(participant, completion):
+    answer = ''
+    return answer
+
+
+
+
+
+print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
+print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
+print(solution([["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]]))
+
+# answer
+# "leo"
+# "vinko"
+# "mislav"
