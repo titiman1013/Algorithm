58f0a2c3a7abfcc59ab3b97392f582774c0cdf48
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-07 15:52:44 +0900

    21.01.07 problem3

---
+++ 21.01.07/Programmers_42578.py
@@ -0,0 +1,15 @@
+def solution(clothes):
+    answer = 0
+    return answer
+
+
+
+
+
+print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
+print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]))
+
+# answer
+
+# 5
+# 3
