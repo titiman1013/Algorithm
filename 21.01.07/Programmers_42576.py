15312955f725826e90412cdc32fb641b2aef481c
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-07 10:39:28 +0900

    21.01.07 problem1 add best solve

--- 21.01.07/Programmers_42576.py
+++ 21.01.07/Programmers_42576.py
@@ -1,21 +1,32 @@
-from collections import Counter
+# first solve
 
+# from collections import Counter
 
-def solution(participant, completion):
-    participant_lst = Counter(participant)
-    completion_lst = Counter(completion)
+# def solution(participant, completion):
+#     participant_lst = Counter(participant)
+#     completion_lst = Counter(completion)
 
-    if len(participant_lst) != len(completion_lst):
-        for key in participant_lst.keys():
-            if key not in completion_lst:
-                return key
-    else:
-        for key, val in participant_lst.items():
-            if completion_lst[key] != val:
-                return key
+#     if len(participant_lst) != len(completion_lst):
+#         for key in participant_lst.keys():
+#             if key not in completion_lst:
+#                 return key
+#     else:
+#         for key, val in participant_lst.items():
+#             if completion_lst[key] != val:
+#                 return key
 
     # answer = ''
     # return answer
+
+
+
+# best solve
+
+from collections import Counter
+
+def solution(participant, completion):
+    answer = Counter(participant) - Counter(completion)
+    return list(answer.keys())[0]
 
 
 
