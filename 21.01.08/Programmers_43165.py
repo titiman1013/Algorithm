545227039917f13058ec58b0a3a2709567da1854
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-08 11:01:49 +0900

    21.01.08 problem1 clear

--- 21.01.08/Programmers_43165.py
+++ 21.01.08/Programmers_43165.py
@@ -1,7 +1,34 @@
-def solution(numbers, target):
-    answer = 0
-    return answer
+# recursive solve
 
+# def solution(numbers, target):
+#     global answer
+#     answer = 0
+#     recursive(numbers, target, 0, 0)
+#     return answer
+
+
+# def recursive(numbers, target, cnt, val):
+#     global answer
+
+#     if cnt == len(numbers):
+#         if val == target:
+#             answer += 1
+#         return
+        
+
+#     recursive(numbers, target, cnt + 1, val + numbers[cnt])
+#     recursive(numbers, target, cnt + 1, val - numbers[cnt])
+
+
+
+# product module
+
+from itertools import product
+
+def solution(numbers, target):
+    lst = [[x, -x] for x in numbers]
+    sum_lst = list(map(sum, product(*lst)))
+    return sum_lst.count(target)
 
 
 
