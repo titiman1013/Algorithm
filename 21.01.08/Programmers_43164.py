1f2f638ad963138cd486c41ecfc35ed7d2e25407
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-08 11:04:22 +0900

    21.01.08 problem2

---
+++ 21.01.08/Programmers_43164.py
@@ -0,0 +1,9 @@
+def solution(tickets):
+    answer = []
+    return answer
+
+
+
+
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]))
