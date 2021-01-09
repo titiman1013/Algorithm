794eddd6c69af424828e58bd93e5a1edde33bb0e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-09 21:43:28 +0900

    21.01.09 problem1

--- 21.01.08/Programmers_43164.py
+++ 21.01.08/Programmers_43164.py
@@ -1,6 +1,23 @@
 def solution(tickets):
+    global answer
     answer = []
+    K = len(tickets)
+    recursive(tickets, "ICN", ["ICN"], K, 0, [0] * K)
     return answer
+
+
+def recursive(tickets, now, lst, K, cnt, visited):
+    if cnt == K:
+        answer.append(lst)
+        return
+
+    for i in range(len(tickets)):
+        if tickets[i][0] == now and visited[i] == 0:
+            visited[i] = 1
+            lst.append(tickets[i][1])
+            recursive(tickets, tickets[i][1], lst, K, cnt + 1, visited)
+
+    return
 
 
 
