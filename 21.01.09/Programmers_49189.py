2650379c9fe61acc8c14f31caf41a0f00e6e976d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-10 04:30:39 +0900

    21.01.09 problem1 time error

--- 21.01.09/Programmers_49189.py
+++ 21.01.09/Programmers_49189.py
@@ -1,5 +1,30 @@
+from collections import deque
+
 def solution(n, edge):
-    answer = 0
+    arr = [[0] * (n + 1) for _ in range(n + 1)]
+    for x, y in edge:
+        arr[x][y] = 1
+        arr[y][x] = 1
+    
+    visited = [0] * (n + 1)
+    d = deque()
+    d.append(1)
+    visited[1] = 1
+    while d:
+        x = d.popleft()
+        for k in range(1, n + 1):
+            if arr[x][k] and visited[k] == 0:
+                visited[k] = visited[x] + 1
+                d.append(k)
+    
+    max_lst = []
+    max_len = max(visited)
+    for i in range(1, n + 1):
+        if visited[i] == max_len:
+            max_lst.append(i)
+    
+    answer = len(max_lst)
+
     return answer
 
 
