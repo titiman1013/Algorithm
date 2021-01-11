350448bea1e36572db7e50211e3fe1058b74113b
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-11 16:46:41 +0900

    21.01.11 problem1 clear

--- 21.01.11/Programmers_43238.py
+++ 21.01.11/Programmers_43238.py
@@ -1,7 +1,20 @@
 def solution(n, times):
     answer = 0
-    return answer
 
+    low, high = (n * min(times)) // len(times), n * max(times)
+    while low <= high:
+        mid = (low + high) // 2
+        temp = 0
+        for time in times:
+            temp += mid // time
+            if temp >= n:
+                answer = mid
+                high = mid - 1
+                break
+        if temp < n:
+            low = mid + 1
+
+    return answer
 
 
 
