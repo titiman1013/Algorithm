cb53f051cffe7890ce4c5eeeebf14bdd6d05295c
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-12 11:09:04 +0900

    21.01.12 problem2

---
+++ 21.01.12/Programmers_42586.py
@@ -0,0 +1,9 @@
+def solution(progresses, speeds):
+    answer = []
+    return answer
+
+
+
+
+print(solution([93, 30, 55], [1, 30, 5]))
+print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
