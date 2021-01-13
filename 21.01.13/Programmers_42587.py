17355ac1810c602f2dbb636ed83df696b5bf89a3
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-13 17:28:01 +0900

    21.01.13 problem2 clear

--- 21.01.13/Programmers_42587.py
+++ 21.01.13/Programmers_42587.py
@@ -1,5 +1,30 @@
+from collections import deque, defaultdict
+
 def solution(priorities, location):
-    answer = 0
+    order_lst = []
+
+    N = len(priorities)
+    d_p = deque(priorities)
+    d_o = deque(i for i in range(N))
+
+    while d_p:
+        if len(d_p) == 1:
+            order_lst.append(d_o[0])
+            break
+
+        prior = d_p.popleft()
+        order_num = d_o.popleft()
+        if prior >= max(d_p):
+            order_lst.append(order_num)
+        else:
+            d_p.append(prior)
+            d_o.append(order_num)
+
+    for idx, val in enumerate(order_lst):
+        if val == location:
+            answer = idx + 1
+            break
+
     return answer
 
 
