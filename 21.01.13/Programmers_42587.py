370e0f5de6c616382c1839f20e9fd3ab2c499569
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-13 17:36:12 +0900

    21.01.13 problem2 add best solve

--- 21.01.13/Programmers_42587.py
+++ 21.01.13/Programmers_42587.py
@@ -1,31 +1,48 @@
-from collections import deque, defaultdict
+# from collections import deque
+
+# def solution(priorities, location):
+#     order_lst = []
+
+#     N = len(priorities)
+#     d_p = deque(priorities)
+#     d_o = deque(i for i in range(N))
+
+#     while d_p:
+#         if len(d_p) == 1:
+#             order_lst.append(d_o[0])
+#             break
+
+#         prior = d_p.popleft()
+#         order_num = d_o.popleft()
+#         if prior >= max(d_p):
+#             order_lst.append(order_num)
+#         else:
+#             d_p.append(prior)
+#             d_o.append(order_num)
+
+#     for idx, val in enumerate(order_lst):
+#         if val == location:
+#             answer = idx + 1
+#             break
+
+#     return answer
+
+
+
+# best solve
+from collections import deque
 
 def solution(priorities, location):
-    order_lst = []
-
-    N = len(priorities)
-    d_p = deque(priorities)
-    d_o = deque(i for i in range(N))
-
-    while d_p:
-        if len(d_p) == 1:
-            order_lst.append(d_o[0])
-            break
-
-        prior = d_p.popleft()
-        order_num = d_o.popleft()
-        if prior >= max(d_p):
-            order_lst.append(order_num)
+    answer = 0
+    deq = deque((i, p) for i, p in enumerate(priorities))
+    while True:
+        cur = deq.popleft()
+        if any(cur[1] < d[1] for d in deq):
+            deq.append(cur)
         else:
-            d_p.append(prior)
-            d_o.append(order_num)
-
-    for idx, val in enumerate(order_lst):
-        if val == location:
-            answer = idx + 1
-            break
-
-    return answer
+            answer += 1
+            if cur[0] == location:
+                return answer
 
 
 
