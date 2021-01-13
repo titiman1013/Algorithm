05deabbef7653e9fb95ad1101356751f0e9ef0f2
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-13 11:29:19 +0900

    21.01.12 problem2 clear

--- 21.01.12/Programmers_42586.py
+++ 21.01.12/Programmers_42586.py
@@ -1,5 +1,28 @@
 def solution(progresses, speeds):
     answer = []
+
+    N = len(progresses)
+    days = [0] * N
+    for i in range(N):
+        if (100 - progresses[i]) % speeds[i]:
+            days[i] = (100 - progresses[i]) // speeds[i] + 1
+        else:
+            days[i] = (100 - progresses[i]) // speeds[i]
+    
+    start = days[0]
+    idx = 1
+    cnt = 1
+    while idx < N:
+        if start >= days[idx]:
+            cnt += 1
+        else:
+            start = days[idx]
+            answer.append(cnt)
+            cnt = 1
+        idx += 1
+        if idx == N:
+            answer.append(cnt)
+
     return answer
 
 
