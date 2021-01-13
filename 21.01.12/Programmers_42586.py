dbab9f6814003dbf22eb8e679a34a01354cd5d5e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-13 11:41:23 +0900

    21.01.12 problem2 add best solve

--- 21.01.12/Programmers_42586.py
+++ 21.01.12/Programmers_42586.py
@@ -26,6 +26,17 @@ def solution(progresses, speeds):
     return answer
 
 
+# best solve
+
+# def solution(progresses, speeds):
+#     Q=[]
+#     for p, s in zip(progresses, speeds):
+#         if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+#             Q.append([-((p-100)//s),1])
+#         else:
+#             Q[-1][1]+=1
+#     return [q[1] for q in Q]
+
 
 
 print(solution([93, 30, 55], [1, 30, 5]))
