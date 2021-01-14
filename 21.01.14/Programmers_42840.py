dff24faeea0e18995af637457eba3dda20fcd9f2
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-14 11:12:45 +0900

    21.01.14 problem1 clear

--- 21.01.14/Programmers_42840.py
+++ 21.01.14/Programmers_42840.py
@@ -1,5 +1,23 @@
 def solution(answers):
     answer = []
+
+    A = [1, 2, 3, 4, 5]
+    B = [2, 1, 2, 3, 2, 4, 2, 5]
+    C = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    cnts = [0] * 3
+    for i in range(len(answers)):
+        if answers[i] == A[i % 5]:
+            cnts[0] += 1
+        if answers[i] == B[i % 8]:
+            cnts[1] += 1
+        if answers[i] == C[i % 10]:
+            cnts[2] += 1
+    
+    for i in range(len(cnts)):
+        if cnts[i] == max(cnts):
+            answer.append(i + 1)
+
     return answer
 
 
