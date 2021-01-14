ab4f18ba405e611c045df7a0fc02843ec72b6190
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-14 15:17:48 +0900

    21.01.14 problem3 add best solve

--- 21.01.14/Programmers_42842.py
+++ 21.01.14/Programmers_42842.py
@@ -1,14 +1,24 @@
+# def solution(brown, yellow):
+#     answer = []
+
+#     all = brown + yellow
+#     for i in range(min(3, min(brown, yellow)), max(brown, yellow)):
+#         if all / i == all // i:
+#             if (all // i + i - 2) * 2  == brown and all // i >= i:
+#                 answer = [all // i, i]
+#                 break
+
+#     return answer
+
+
+
+# best solve
+
 def solution(brown, yellow):
-    answer = []
-
-    all = brown + yellow
-    for i in range(min(3, min(brown, yellow)), max(brown, yellow)):
-        if all / i == all // i:
-            if (all // i + i - 2) * 2  == brown and all // i >= i:
-                answer = [all // i, i]
-                break
-
-    return answer
+    for i in range(1, int(yellow ** (1 / 2)) + 1):
+        if yellow % i == 0:
+            if (i + yellow // i) * 2 == brown - 4:
+                return [yellow // i + 2, i + 2]
 
 
 
