0a81433d3782e54696b41aa3b7683b4da1e1e548
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-14 15:02:52 +0900

    21.01.14 problem3 clear

--- 21.01.14/Programmers_42842.py
+++ 21.01.14/Programmers_42842.py
@@ -1,5 +1,13 @@
 def solution(brown, yellow):
     answer = []
+
+    all = brown + yellow
+    for i in range(min(3, min(brown, yellow)), max(brown, yellow)):
+        if all / i == all // i:
+            if (all // i + i - 2) * 2  == brown and all // i >= i:
+                answer = [all // i, i]
+                break
+
     return answer
 
 
