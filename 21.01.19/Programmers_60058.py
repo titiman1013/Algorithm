9cb65d97b62d665e92d17ecaef3ee3e4c82b2d38
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-19 15:39:27 +0900

    21.01.19 problem1 false

--- 21.01.19/Programmers_60058.py
+++ 21.01.19/Programmers_60058.py
@@ -1,6 +1,58 @@
-def solution(p):
-    answer = ''
-    return answer
+# false
+
+# from collections import deque
+
+# def solution(p):
+#     answer = ''
+
+#     w = p
+#     u, v = check(w)
+#     if len(u) != len(p):
+#         u, v = revise(u, v)
+
+#     return u
+
+
+
+# def check(w):
+#     stack = []
+#     for i in range(len(w)):
+#         if w[i] == '(':
+#             stack.append('(')
+#         else:
+#             if len(stack) == 0:
+#                 return w[:i], w[i:]
+#             else:
+#                 stack.pop()
+#     return w, ''
+
+
+# def revise(u, v):
+#     deq = deque(v)
+#     temp_open = []
+#     temp_close = []
+#     while deq:
+#         val = deq.popleft()
+#         if val == '(':
+#             if len(temp_close):
+#                 temp_open.append('(')
+#                 if len(temp_open) == len(temp_close):
+#                     for i in range(len(temp_open)):
+#                         u += temp_open.pop()
+#                     for i in range(len(temp_close)):
+#                         u += temp_close.pop()
+#             else:
+#                 deq.appendleft(val)
+#                 temp = ''
+#                 for i in range(len(deq)):
+#                     temp += deq[i]
+#                 temp_u, temp_v = check(temp)
+#                 u += ''.join(temp_u)
+#                 deq = deque(temp_v)
+#         else:
+#             temp_close.append(')')
+#     return u, deq
+        
 
 
 
