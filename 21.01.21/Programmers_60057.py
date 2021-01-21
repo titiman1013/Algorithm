036276e24380430edbbca7dda463fdb0adc8b8d4
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-21 12:36:43 +0900

    21.01.21 problem1 clear

--- 21.01.21/Programmers_60057.py
+++ 21.01.21/Programmers_60057.py
@@ -1,5 +1,32 @@
 def solution(s):
-    answer = 0
+    answer = 1001
+
+    for num in range(1, len(s) // 2 + 1):
+        string = ''
+        for i in range(0, len(s), num):
+            if i == 0:
+                unit = s[:i + num]
+                cnt = 1
+            else:
+                if unit == s[i:i + num]:
+                    cnt += 1
+                else:
+                    if cnt > 1:
+                        string += str(cnt) + unit
+                    else:
+                        string += unit
+                    unit = s[i:i + num]
+                    cnt = 1
+                if i + num >= len(s):
+                    if cnt > 1:
+                        string += str(cnt)
+                    string += unit
+        if len(string) < answer:
+            answer = len(string)
+
+    if answer == 1001:
+        answer = len(s)
+
     return answer
 
 
