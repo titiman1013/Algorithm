b4d0636ba5e83b8914c7af1f70d111316996a731
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-22 11:38:22 +0900

    21.01.22 problem1 clear

--- 21.01.22/Programmers_42626.py
+++ 21.01.22/Programmers_42626.py
@@ -1,5 +1,23 @@
+import heapq
+
 def solution(scoville, K):
     answer = 0
+
+    # init
+    heapq.heapify(scoville)
+
+    min_val = scoville[0]
+    while min_val < K:
+        val1 = heapq.heappop(scoville)
+        val2 = heapq.heappop(scoville)
+        val3 = val1 + val2 * 2
+        heapq.heappush(scoville, val3)
+        min_val = scoville[0]
+        answer += 1
+        if len(scoville) < 2:
+            answer = -1
+            break
+
     return answer
 
 
