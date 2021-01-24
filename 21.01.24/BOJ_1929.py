24a99508e1eaa04bff23166a1a5f1a6918a1bf97
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-24 11:31:17 +0900

    21.01.24 problem2 clear

--- 21.01.24/BOJ_1929.py
+++ 21.01.24/BOJ_1929.py
@@ -1,5 +1,16 @@
 import sys; sys.stdin = open('text1.txt', 'r')
+import math
+
+def check(num):
+    if num == 1: return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0: return False
+    return True
+
+
 
 for tc in range(1, int(input()) + 1):
     M, N = map(int, input().split())
-    
+    for num in range(M, N + 1):
+        if check(num):
+            print(num)
