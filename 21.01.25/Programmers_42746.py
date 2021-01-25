5721770cf629bd4c137d72196273dc410048aeb1
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-25 17:26:43 +0900

    21.01.25 problem2 time error

--- 21.01.25/Programmers_42746.py
+++ 21.01.25/Programmers_42746.py
@@ -1,5 +1,20 @@
+# time error
+
+from itertools import permutations
+
 def solution(numbers):
     answer = ''
+
+    max = 0
+    number_lst = list(set(permutations(numbers, len(numbers))))
+    for number in number_lst:
+        temp = ''
+        for num in number:
+            temp += str(num)
+        if int(temp) > max:
+            max = int(temp)
+    answer = str(max)
+
     return answer
 
 
@@ -7,3 +22,7 @@ def solution(numbers):
 
 print(solution([6, 10, 2]))
 print(solution([3, 30, 34, 5, 9]))
+
+# answer
+# 6210
+# 9534330
