e84b7729ddee575987ed3dfc52909d8489887f18
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-26 16:28:54 +0900

    21.01.25 problem1 clear

--- 21.01.25/Programmers_42746.py
+++ 21.01.25/Programmers_42746.py
@@ -23,7 +23,10 @@
 def solution(numbers):
     answer = ''
 
-    
+    numbers = list(map(str, numbers))
+    # 문자열 비교연산은 앞자리부터 ascii코드로 진행되며, 1000이하의 수가 조건이기 때문에 *3을 해준다
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    answer += str(int(''.join(numbers)))
 
     return answer
 
