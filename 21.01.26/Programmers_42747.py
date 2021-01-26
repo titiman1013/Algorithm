24220cef039ac31667cf4f971d0ae46bfc8834c7
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-26 11:55:25 +0900

    21.01.26 problem1 clear

--- 21.01.26/Programmers_42747.py
+++ 21.01.26/Programmers_42747.py
@@ -1,5 +1,31 @@
+# from collections import Counter
+
+# def solution(citations):
+#     answer = 0
+
+#     citations.sort(reverse=True)
+#     dic = Counter(citations)
+    
+#     cnt = 0
+#     for key, val in dic.items():
+#         cnt += val
+#         if key >= cnt :
+#             answer = key
+#             break
+
+#     return answer
+
+
+#
+
 def solution(citations):
     answer = 0
+
+    citations.sort(reverse=True)
+    for idx, citation in enumerate(citations):
+        if idx + 1 <= citation:
+            answer = idx + 1
+
     return answer
 
 
@@ -7,6 +33,8 @@ def solution(citations):
 
 
 print(solution([3, 0, 6, 1, 5]))
+print(solution([31, 66]))
 
 # answer 
 # 3
+# 2
