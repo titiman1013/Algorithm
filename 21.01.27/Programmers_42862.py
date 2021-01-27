6f0fdba497d31a39789772d83bcab7d5fc40a183
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-27 10:15:04 +0900

    21.01.27 problem1

---
+++ 21.01.27/Programmers_42862.py
@@ -0,0 +1,15 @@
+def solution(n, lost, reserve):
+    answer = 0
+    return answer
+
+
+
+
+print(solution(5, [2, 4], [1, 3, 5]))
+print(solution(5, [2, 4], [3]))
+print(solution(3, [3], [1]))
+
+# answer
+# 5
+# 4
+# 2
