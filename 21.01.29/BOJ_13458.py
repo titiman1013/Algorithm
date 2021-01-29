e85557365316d2bbb7c3adbf073cae884e7e8908
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-29 16:43:25 +0900

    21.01.29 problem1 clear

--- 21.01.29/BOJ_13458.py
+++ 21.01.29/BOJ_13458.py
@@ -4,3 +4,15 @@ for tc in range(1, int(input()) + 1):
     N = int(input())
     A = list(map(int, input().split()))
     B, C = map(int, input().split())
+
+    answer = 0
+    for room in A:
+        answer += 1
+        room -= B
+        if room > 0:
+            if room % C:
+                answer += room // C + 1
+            else:
+                answer += room // C
+    
+    print(answer)
