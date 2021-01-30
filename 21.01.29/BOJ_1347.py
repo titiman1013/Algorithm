09cc1498661b2f7a1ef4b15cf289eefd47e806c5
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-30 10:40:02 +0900

    21.01.30 problem1

--- 21.01.29/BOJ_1347.py
+++ 21.01.29/BOJ_1347.py
@@ -1,5 +1,20 @@
 import sys; sys.stdin = open('text2.txt', 'r')
 
+def search_dot():
+    pass
+
+
+def create_arr():
+    pass
+
+
+def print_arr():
+    pass
+
+
+
 for tc in range(1, int(input()) + 1):
     N = int(input())
     orders = input()
+
+    
---
+++ 21.01.30/SWEA_1217.py
@@ -0,0 +1,5 @@
+import sys; sys.stdin = open('text1.txt', 'r')
+
+for _ in range(1, 11):
+    tc = int(input())
+    N, M = map(int, input().split())
