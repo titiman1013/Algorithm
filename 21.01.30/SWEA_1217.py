018ac87f98697ea3560f41d7410faa33769c9e23
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-01-30 13:00:12 +0900

    21.01.30 problem1 clear

--- 21.01.30/SWEA_1217.py
+++ 21.01.30/SWEA_1217.py
@@ -1,5 +1,20 @@
 import sys; sys.stdin = open('text1.txt', 'r')
 
+def reculsive(N, M, res):
+    global answer
+
+    if M == 0:
+        answer = res
+        return
+    
+    reculsive(N, M - 1, res * N)
+
+
+
 for _ in range(1, 11):
     tc = int(input())
     N, M = map(int, input().split())
+
+    answer = 0 
+    reculsive(N, M, 1)
+    print(f'#{tc} {answer}')
