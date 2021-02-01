1e7e66d53357ce774ec342efda047aa8a9fcb4f1
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-01 11:26:37 +0900

    21.02.01 problem2 clear

--- 21.02.01/SWEA_10505.py
+++ 21.02.01/SWEA_10505.py
@@ -3,3 +3,10 @@ import sys; sys.stdin = open('10505.txt', 'r')
 for tc in range(1, int(input()) + 1):
     N = int(input())
     arr = list(map(int, input().split()))
+
+    answer = 0
+    avg = sum(arr) // N + 1
+    for i in range(N):
+        if arr[i] < avg:
+            answer += 1
+    print(f'#{tc} {answer}')
