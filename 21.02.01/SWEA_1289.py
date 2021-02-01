8be119c2846fc794c488b533920f320e90b3aa26
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-01 11:00:37 +0900

    21.02.01 problem1 clear

--- 21.02.01/SWEA_1289.py
+++ 21.02.01/SWEA_1289.py
@@ -2,3 +2,14 @@ import sys; sys.stdin = open('1289.txt', 'r')
 
 for tc in range(1, int(input()) + 1):
     ori = input()
+
+    res = '0' * len(ori)
+    answer = 0
+    for i in range(len(ori)):
+        if res[i] != ori[i]:
+            temp = res[:i]
+            res = temp + ori[i] * (len(ori) - len(temp))
+            answer += 1
+        
+        if res == ori: break
+    print(f'#{tc} {answer}')
