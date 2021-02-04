4a740241009e1cc0e9ea9195c43521853951e3c8
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-04 11:13:24 +0900

    21.02.04 problem1 clear

--- 21.02.04/SWEA_1213.py
+++ 21.02.04/SWEA_1213.py
@@ -4,3 +4,13 @@ for _ in range(1, 11):
     tc = int(input())
     target = input()
     strings = input()
+
+    answer = 0
+    idx = 0
+    while idx <= len(strings) - len(target):
+        if strings[idx:idx+len(target)] == target:
+            answer += 1
+            idx += len(target)
+        else:
+            idx += 1
+    print(f'#{tc} {answer}')
