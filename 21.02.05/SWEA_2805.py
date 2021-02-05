d14fbda0fc97b41a13b186408d93e8438e2d5cf7
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-05 10:57:49 +0900

    21.02.05 problem1 clear

--- 21.02.05/SWEA_2805.py
+++ 21.02.05/SWEA_2805.py
@@ -3,3 +3,13 @@ import sys; sys.stdin = open('2805.txt', 'r')
 for tc in range(1, int(input()) + 1):
     N = int(input())
     arr = [list(map(int, input())) for _ in range(N)]
+
+    answer = 0
+    for i in range(N // 2):
+        for j in range(N // 2 - i, N // 2 + i + 1):
+            answer += arr[i][j] + arr[-i - 1][j]
+    if N > 1:
+        answer += sum(arr[N // 2])
+    else:
+        answer += arr[0][0]
+    print(f'#{tc} {answer}')
