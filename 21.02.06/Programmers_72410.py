113899d05d9d659cb1e9eef25fa8f5d816eeab0a
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-06 13:20:36 +0900

    21.02.06 problem1

---
+++ 21.02.06/Programmers_72410.py
@@ -0,0 +1,12 @@
+def solution(new_id):
+    answer = ''
+    return answer
+
+
+
+
+print(solution("...!@BaT#*..y.abcdefghijklm"))
+print(solution(	"z-+.^."))
+print(solution("=.="))
+print(solution("123_.def"))
+print(solution("abcdefghijklmn.p"))
