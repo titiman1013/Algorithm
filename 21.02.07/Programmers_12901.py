bd3bfadec7dd11276adaf70f46405cd392b62167
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-07 15:29:33 +0900

    21.02.07 problem1 clear

--- 21.02.07/Programmers_12901.py
+++ 21.02.07/Programmers_12901.py
@@ -1,5 +1,23 @@
 def solution(a, b):
     answer = ''
+
+    t0 = [4, 6, 9, 11]
+    t1 = [1, 3, 5, 7, 8, 10, 12]
+    t9 = [2]
+    day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+
+    date = 0
+    for i in range(1, a):
+        if i in t0:
+            date += 30
+        elif i in t1:
+            date += 31
+        else:
+            date += 29
+    date += b - 1
+
+    answer = day[(5 + date) % 7]
+
     return answer
 
 
