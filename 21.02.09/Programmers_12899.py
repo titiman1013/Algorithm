7ccea7d41994a09c7d1c9aff6e06f06ba86d479a
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-09 10:36:39 +0900

    21.02.09 problem1

---
+++ 21.02.09/Programmers_12899.py
@@ -0,0 +1,22 @@
+def solution(n):
+    answer = 0
+    return answer
+
+
+
+
+print(solution(1))
+print(solution(2))
+print(solution(3))
+print(solution(4))
+print(solution(5))
+print(solution(6))
+print(solution(7))
+print(solution(8))
+print(solution(9))
+
+# answer
+# 1
+# 2
+# 4
+# 11
