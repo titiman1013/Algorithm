378a9a2435d02ee7e922f5587a67a2a239e4f7d6
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-09 11:03:17 +0900

    21.02.09 problem1 clear

--- 21.02.09/Programmers_12899.py
+++ 21.02.09/Programmers_12899.py
@@ -1,6 +1,9 @@
 def solution(n):
-    answer = 0
-    return answer
+    if n <= 3:
+        return '124'[n - 1]
+    else:
+        div, mod = divmod(n - 1, 3)
+        return solution(div) + '124'[mod]
 
 
 
