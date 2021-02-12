a7c3b555b4785a84f93fcaf3b0ff23d2020851d7
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-12 17:23:43 +0900

    21.02.12 problem1 clear

--- 21.02.12/SWEA_1983.py
+++ 21.02.12/SWEA_1983.py
@@ -5,4 +5,22 @@ grades = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
 for tc in range(1, int(input()) + 1):
     # N: 학생수, K: 학생 번호
     N, K = map(int, input().split())
-    scores = [list(map(int, input().split())) for _ in range(N)]
+    # scores = [list(map(int, input().split())) for _ in range(N)]
+
+    scores = []
+    idx = 1
+    for _ in range(N):
+        mid, fin, rep = map(int, input().split())
+        scores.append([mid * 0.35 + fin * 0.45 + rep * 0.2, idx])
+        idx += 1
+
+    scores.sort(reverse=True)
+    for idx, student in enumerate(scores):
+        if student[1] == K:
+            break
+    
+    tmp = N // 10
+    if (idx + 1) % tmp:
+        print(f'#{tc} {grades[(idx + 1) // tmp]}')
+    else:
+        print(f'#{tc} {grades[(idx + 1) // tmp - 1]}')
