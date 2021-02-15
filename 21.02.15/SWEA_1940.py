ab4cf6f68c7e6513e95b261cbc80263ec9f13864
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-15 11:35:27 +0900

    21.02.15 problem1 clear

--- 21.02.15/SWEA_1940.py
+++ 21.02.15/SWEA_1940.py
@@ -3,3 +3,21 @@ import sys; sys.stdin = open('1940.txt', 'r')
 for tc in range(1, int(input()) + 1):
     N = int(input())
     commands = [list(map(int, input().split())) for _ in range(N)]
+
+    answer = 0
+    vel = 0
+    for command in commands:
+        if command[0] == 0:
+            if vel:
+                answer += vel
+        elif command[0] == 1:
+            vel += command[1]
+            answer += vel
+        elif command[0] == 2:
+            vel -= command[1]
+            if vel < 0:
+                vel = 0
+                continue
+            answer += vel
+    
+    print(f'#{tc} {answer}')
