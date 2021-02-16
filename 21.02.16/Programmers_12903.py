e689373ed9f44a8cf5aaa20ff9cb183f97f827d4
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-16 10:33:51 +0900

    21.02.16 problem1 clear

--- 21.02.16/Programmers_12903.py
+++ 21.02.16/Programmers_12903.py
@@ -1,5 +1,12 @@
 def solution(s):
     answer = ''
+
+    length = len(s)
+    if length % 2:
+        answer = s[length // 2]
+    else:
+        answer = s[length // 2 - 1:length // 2 + 1]
+
     return answer
 
 
