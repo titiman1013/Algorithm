463ff7c0ff2f50d28520f9ab5131138827d291b2
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-16 10:49:30 +0900

    21.02.16 problem1 add short code

--- 21.02.16/Programmers_12903.py
+++ 21.02.16/Programmers_12903.py
@@ -1,13 +1,20 @@
+# def solution(s):
+#     answer = ''
+
+#     length = len(s)
+#     if length % 2:
+#         answer = s[length // 2]
+#     else:
+#         answer = s[length // 2 - 1:length // 2 + 1]
+
+#     return answer
+
+
+# short coding
+
 def solution(s):
-    answer = ''
+    return str[(len(s) - 1) // 2:len(s) // 2 + 1]
 
-    length = len(s)
-    if length % 2:
-        answer = s[length // 2]
-    else:
-        answer = s[length // 2 - 1:length // 2 + 1]
-
-    return answer
 
 
 
