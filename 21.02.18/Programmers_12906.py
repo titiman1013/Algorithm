ce0c36d72d03be2873a919dd48c892fde45e5309
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-18 10:40:26 +0900

    21.02.18 problem1 add solve

--- 21.02.18/Programmers_12906.py
+++ 21.02.18/Programmers_12906.py
@@ -1,13 +1,26 @@
+# def solution(arr):
+#     answer = []
+
+#     for val in arr:
+#         if answer:
+#             if answer[-1] == val:
+#                 continue
+#         answer.append(val)
+
+#     return answer
+
+
+
+# slicing
+
+# 빈 array을 슬라이싱해도 error가 발생하지 않음
+
 def solution(arr):
-    answer = []
-
-    for val in arr:
-        if answer:
-            if answer[-1] == val:
-                continue
-        answer.append(val)
-
-    return answer
+    a = []
+    for i in arr:
+        if a[-1:] == [i]: continue
+        a.append(i)
+    return a
 
 
 
