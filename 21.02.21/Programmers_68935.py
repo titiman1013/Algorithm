5319d1fbed59feace7c4a500db9910b48e78afd8
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-21 13:18:08 +0900

    21.02.21 problem1 clear

--- 21.02.21/Programmers_68935.py
+++ 21.02.21/Programmers_68935.py
@@ -1,5 +1,22 @@
+def trans_reverse_ternary(num):
+    res = ''
+    while num > 0:
+        quotient, remainder = divmod(num, 3)
+        res += str(remainder)
+        num = quotient
+    return res
+
+
+def trans_decimal(num):
+    # int(변환할 숫자, 변환할 숫자의 진법) => 변환할 숫자를 10진법으로 바꿔줌
+    return int(num, 3)
+
+
 def solution(n):
     answer = 0
+
+    answer = trans_decimal(trans_reverse_ternary(n))
+
     return answer
 
 
