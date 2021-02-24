f9f199d2bcbadbf892b6e69ec39d8f25d6f5c320
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-24 11:56:27 +0900

    21.02.24 problem1

---
+++ 21.02.24/Programmers_12915.py
@@ -0,0 +1,13 @@
+def solution(strings, n):
+    answer = []
+    return answer
+
+
+
+
+print(solution(["sun", "bed", "car"], 1))
+print(solution(["abce", "abcd", "cdx"], 2))
+
+# answer
+# ["car", "bed", "sun"]
+# ["abcd", "abce", "cdx"]
