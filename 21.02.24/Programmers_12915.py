5566a26a799e64b9603fed8d7402c385c5ba97ba
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-24 14:12:41 +0900

    21.02.24 problem1 add short code

--- 21.02.24/Programmers_12915.py
+++ 21.02.24/Programmers_12915.py
@@ -1,29 +1,31 @@
 def solution(strings, n):
-    answer = []
+    # answer = []
 
-    order_dict = {}
-    order_list = []
-    for idx, string in enumerate(strings):
-        if order_dict.get(string[n]):
-            order_dict[string[n]].append(idx)
-        else:
-            order_dict[string[n]] = [idx]
-            order_list.append(string[n])
+    # order_dict = {}
+    # order_list = []
+    # for idx, string in enumerate(strings):
+    #     if order_dict.get(string[n]):
+    #         order_dict[string[n]].append(idx)
+    #     else:
+    #         order_dict[string[n]] = [idx]
+    #         order_list.append(string[n])
     
-    order_list.sort()
-    # print(order_list)
+    # order_list.sort()
+    # # print(order_list)
 
-    for string in order_list:
-        vals = order_dict.get(string)
-        if len(vals) > 1:
-            temps = [(strings[val], val) for val in vals]
-            temps.sort()
-            vals = [temp[1] for temp in temps]
+    # for string in order_list:
+    #     vals = order_dict.get(string)
+    #     if len(vals) > 1:
+    #         temps = [(strings[val], val) for val in vals]
+    #         temps.sort()
+    #         vals = [temp[1] for temp in temps]
         
-        for val in vals:
-            answer.append(strings[val])
+    #     for val in vals:
+    #         answer.append(strings[val])
 
-    return answer
+    # return answer
+
+    return sorted(strings, key=lambda x: x[n])
 
 
 
