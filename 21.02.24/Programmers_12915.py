9324bc9ceb6a2b53bccfd50a9c385f5a58b92d3f
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-24 13:59:03 +0900

    21.02.24 problem1 clear

--- 21.02.24/Programmers_12915.py
+++ 21.02.24/Programmers_12915.py
@@ -1,5 +1,28 @@
 def solution(strings, n):
     answer = []
+
+    order_dict = {}
+    order_list = []
+    for idx, string in enumerate(strings):
+        if order_dict.get(string[n]):
+            order_dict[string[n]].append(idx)
+        else:
+            order_dict[string[n]] = [idx]
+            order_list.append(string[n])
+    
+    order_list.sort()
+    # print(order_list)
+
+    for string in order_list:
+        vals = order_dict.get(string)
+        if len(vals) > 1:
+            temps = [(strings[val], val) for val in vals]
+            temps.sort()
+            vals = [temp[1] for temp in temps]
+        
+        for val in vals:
+            answer.append(strings[val])
+
     return answer
 
 
