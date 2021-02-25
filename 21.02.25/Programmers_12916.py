73de1a5b90ed61bc3348815fbdd8359c514d5c8c
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-25 10:21:00 +0900

    21.02.25 problem1 clear

--- 21.02.25/Programmers_12916.py
+++ 21.02.25/Programmers_12916.py
@@ -1,5 +1,14 @@
 def solution(s):
     answer = True
+
+    cnt = 0
+    for string in s.lower():
+        if string == 'p': cnt += 1
+        elif string == 'y': cnt -= 1
+    
+    if not cnt == 0:
+        answer = False
+
     return answer
 
 
