3d0e0434fa1ea801137bde44f52f9a30d9bead39
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-27 10:45:29 +0900

    21.02.27 problem1 clear

--- 21.02.27/Programmers_12919.py
+++ 21.02.27/Programmers_12919.py
@@ -1,5 +1,11 @@
 def solution(seoul):
     answer = ''
+
+    for idx, val in enumerate(seoul):
+        if val == "Kim":
+            answer = "김서방은 " + str(idx) + "에 있다"
+            break
+
     return answer
 
 
