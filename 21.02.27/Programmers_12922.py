70581cfccfc9849171b7eb072664bd02f24289e9
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-27 11:00:00 +0900

    21.02.27 problem2 clear

--- 21.02.27/Programmers_12922.py
+++ 21.02.27/Programmers_12922.py
@@ -1,6 +1,15 @@
 def solution(n):
     answer = ''
+
+    for i in range(1, n+1):
+        if i % 2:
+            answer += "수"
+        else:
+            answer += "박"
+
     return answer
+    
+    return "수박" * (n//2) + "수" * (n%2)
 
 
 
