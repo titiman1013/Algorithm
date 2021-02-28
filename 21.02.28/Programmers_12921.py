6ef7a5235d2a6d3aa095b8c8389669df6d9ff40e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-28 18:46:38 +0900

    21.02.28 problem1 add solve

--- 21.02.28/Programmers_12921.py
+++ 21.02.28/Programmers_12921.py
@@ -1,14 +1,23 @@
 import math
 
 def solution(n):
-    answer = 0
+    # answer = 0
 
-    for i in range(2, n+1):
-        for j in range(2, int(math.sqrt(i)) + 1):
-            if i % j == 0: break
-        else: answer += 1
+    # for i in range(2, n+1):
+    #     for j in range(2, int(math.sqrt(i)) + 1):
+    #         if i % j == 0: break
+    #     else: answer += 1
 
-    return answer
+    # return answer
+
+
+    num = set(range(2, n+1))
+
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if i in num:
+            num -= set(range(i*i, n+1, i))
+    
+    return len(num)
 
 
 
