eb00011c8bfff747c52baa94f1c5cdc3228df06e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-02-28 18:30:14 +0900

    21.02.28 problem1 clear

--- 21.02.28/Programmers_12921.py
+++ 21.02.28/Programmers_12921.py
@@ -1,5 +1,13 @@
+import math
+
 def solution(n):
     answer = 0
+
+    for i in range(2, n+1):
+        for j in range(2, int(math.sqrt(i)) + 1):
+            if i % j == 0: break
+        else: answer += 1
+
     return answer
 
 
