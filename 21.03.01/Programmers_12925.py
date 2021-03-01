10a8282426ca9b1e14a2e66952183b28d687490d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-01 10:25:22 +0900

    21.03.01 problem1 clear

--- 21.03.01/Programmers_12925.py
+++ 21.03.01/Programmers_12925.py
@@ -1,5 +1,13 @@
 def solution(s):
     answer = 0
+
+    if s[0] == '-':
+        answer = int(s[1:]) * -1
+    elif s[0] == '+':
+        answer = int(s[1:])
+    else:
+        answer = int(s)
+
     return answer
 
 
