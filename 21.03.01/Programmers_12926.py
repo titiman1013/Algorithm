e7db49ae3c34e8da7ba2380449936f970f92c310
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-01 10:48:57 +0900

    21.03.01 problem2 clear

--- 21.03.01/Programmers_12926.py
+++ 21.03.01/Programmers_12926.py
@@ -1,5 +1,21 @@
 def solution(s, n):
     answer = ''
+
+    s_list = list(map(str, s))
+    for string in s_list:
+        if string == ' ': 
+            answer += ' '
+            continue
+        elif 64 < ord(string) <= 90:
+            temp = ord(string) + n
+            if temp > 90:
+                temp = temp - 90 + 64
+        elif 96 < ord(string) <= 122:
+            temp = ord(string) + n
+            if temp > 122:
+                temp = temp - 122 + 96
+        answer += chr(temp)
+
     return answer
 
 
