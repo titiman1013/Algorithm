538f12a632aea1bd34d3e3326917ed156ec41954
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-01 10:29:51 +0900

    21.03.01 problem1 add short code

--- 21.03.01/Programmers_12925.py
+++ 21.03.01/Programmers_12925.py
@@ -1,14 +1,16 @@
 def solution(s):
-    answer = 0
+    # answer = 0
 
-    if s[0] == '-':
-        answer = int(s[1:]) * -1
-    elif s[0] == '+':
-        answer = int(s[1:])
-    else:
-        answer = int(s)
+    # if s[0] == '-':
+    #     answer = int(s[1:]) * -1
+    # elif s[0] == '+':
+    #     answer = int(s[1:])
+    # else:
+    #     answer = int(s)
 
-    return answer
+    # return answer
+
+    return int(s)
 
 
 
