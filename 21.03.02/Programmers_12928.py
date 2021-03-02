f63c893ec29a9d899fa78e880f009448103a2c6c
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-02 14:41:37 +0900

    21.03.02 problem1 add short code

--- 21.03.02/Programmers_12928.py
+++ 21.03.02/Programmers_12928.py
@@ -1,21 +1,23 @@
 def solution(n):
-    answer = 0
+    # answer = 0
 
-    weaknum_set = set()
-    if n == 1: return 1
-    for num in range(1, n//2+1):
-        # if num not in weaknum_set:
-        div, mod = divmod(n, num)
-        if mod == 0:
-            weaknum_set.add(num)
-            weaknum_set.add(div)
-                # if num == div:
-                #     answer += num
-                # else:
-                #     answer += num + div
-    answer = sum(weaknum_set)
+    # weaknum_set = set()
+    # if n == 1: return 1
+    # for num in range(1, n//2+1):
+    #     # if num not in weaknum_set:
+    #     div, mod = divmod(n, num)
+    #     if mod == 0:
+    #         weaknum_set.add(num)
+    #         weaknum_set.add(div)
+    #             # if num == div:
+    #             #     answer += num
+    #             # else:
+    #             #     answer += num + div
+    # answer = sum(weaknum_set)
 
-    return answer
+    # return answer
+
+    return n + sum([i for i in range(1, (n // 2) + 1) if n % i == 0])
 
 
 
