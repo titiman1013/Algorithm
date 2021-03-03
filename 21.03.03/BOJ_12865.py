f2c800862b44b6a6d6163f5e9c236bf3b9525742
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-03 11:54:04 +0900

    21.03.03 problem4 time error

--- 21.03.03/BOJ_12865.py
+++ 21.03.03/BOJ_12865.py
@@ -1,6 +1,24 @@
 import sys; sys.stdin = open('12865.txt', 'r')
 
-# # N: 물건 개수, K: 들 수 있는 무게
+def reculsive(idx, weight, satis):
+    global answer
+
+    if idx == N:
+        if weight <= K:
+            if satis > answer:
+                answer = satis
+        return
+
+    reculsive(idx+1, weight+loads[idx][0], satis+loads[idx][1])
+    reculsive(idx+1, weight, satis)
+
+
+
+# N: 물건 개수, K: 들 수 있는 무게
 N, K = map(int, input().split())
-# # W: 물건 무게, V: 가치
+# W: 물건 무게, V: 가치
 loads = [list(map(int, input().split())) for _ in range(N)]
+
+answer = 0
+reculsive(0, 0, 0)
+print(answer)
