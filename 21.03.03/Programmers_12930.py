f37c7b30dae5215c3f3cbd51d9408e776d9f12e0
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-03 10:24:19 +0900

    21.03.03 problem2 clear

--- 21.03.03/Programmers_12930.py
+++ 21.03.03/Programmers_12930.py
@@ -1,5 +1,18 @@
 def solution(s):
     answer = ''
+
+    idx = 0
+    for string in s:
+        if string == ' ':
+            idx = 0
+            answer += ' '
+            continue
+        elif idx % 2:
+            answer += string.lower()
+        else:
+            answer += string.upper()
+        idx += 1
+
     return answer
 
 
