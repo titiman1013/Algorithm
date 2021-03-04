68fa6a2ec4e375210ad9f45bf44a2fa1549eb1d2
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-04 10:59:59 +0900

    21.03.04 problem1 clear

--- 21.03.04/BOJ_11758.py
+++ 21.03.04/BOJ_11758.py
@@ -1,7 +1,24 @@
-import sys; sys.stdin = open('text1.txt', 'r')
+import sys; sys.stdin = open('11758.txt', 'r')
+
+def ccw(p1, p2, p3):
+    x1, y1 = p1
+    x2 ,y2 = p2
+    x3, y3 = p3
+    return (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)
+
+
 
 for tc in range(1, int(input()) + 1):
     arr = [list(map(int, input().split())) for _ in range(3)]
+    
+    answer = ccw(*arr)
+
+    if answer < 0:
+        print(-1)
+    elif answer > 0:
+        print(1)
+    else:
+        print(0)
 
 
 
