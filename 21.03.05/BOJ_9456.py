4668d64e210c895f3732dbe780414e1b44ae2835
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-05 11:24:02 +0900

    21.03.05 problem1 clear

--- 21.03.05/BOJ_9456.py
+++ 21.03.05/BOJ_9456.py
@@ -4,6 +4,31 @@ for tc in range(1, int(input()) + 1):
     N = int(input())
     preferences = [0] + list(map(int, input().split()))
 
+    # pre_dicts = [[str(idx+1), pre] for idx, pre in enumerate(preferences)]
+    
+    # res = 0
+    # while preferences:
+    #     num = preferences.pop()
+    #     temp = 1
+    #     if selected[num] == False:
+
+
+    # 연결된 것들은 -1로 확인
+    selected = [0] * (N + 1)
+    team_num = 1
+    for i in range(1, N + 1):
+        if selected[i] == 0:
+            while selected[i] == 0:
+                selected[i] = team_num
+                i = preferences[i]
+            while selected[i] == team_num:
+                selected[i] = -1
+                i = preferences[i]
+            team_num += 1
+    
+    answer = N - selected.count(-1)
+    print(answer)
+
 
 
 
