cdce5a655404b0934d90c601afe9c7a2dc0efec2
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-05 10:49:33 +0900

    21.03.05 problem1

---
+++ 21.03.05/BOJ_9456.py
@@ -0,0 +1,13 @@
+import sys; sys.stdin = open('9466.txt', 'r')
+
+for tc in range(1, int(input()) + 1):
+    N = int(input())
+    preferences = [0] + list(map(int, input().split()))
+
+
+
+
+
+# answer
+# 3
+# 0
