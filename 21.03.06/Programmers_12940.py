9bd773e9aa4844c7b7e150e690a1987cf3412037
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-06 15:49:52 +0900

    21.03.06 problem2 clear

--- 21.03.06/Programmers_12940.py
+++ 21.03.06/Programmers_12940.py
@@ -1,5 +1,25 @@
+# 유클리드 호제법
+
+# 계속 b를 a로, 'a를 b로 나눈 나머지'를 b로 대입시켜 b가 0이 될때 까지 반복하면 남는 a가 최대 공약수
+def GCD(num1, num2):
+    x, y = min(num1, num2), max(num1, num2)
+    while y:
+        x, y = y, x % y
+    return x
+
+
+# 최소 공배수는 a, b의 곱을 최대 공약수로 나눈 것
+def LCM(num1, num2):
+    x, y = min(num1, num2), max(num1, num2)
+    res = (x * y) // GCD(x, y)
+    return res
+
+
 def solution(n, m):
     answer = []
+
+    answer = [GCD(n, m), LCM(n, m)]
+
     return answer
 
 
