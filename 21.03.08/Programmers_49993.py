8bc3fb3d76ebdb6cb38a5a9f3413fe0cd11aad05
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-08 10:49:08 +0900

    21.03.08 problem1 clear

--- 21.03.08/Programmers_49993.py
+++ 21.03.08/Programmers_49993.py
@@ -1,5 +1,22 @@
+def check(answer, skill_tree, sequence):
+    flag = 0
+    for seq in skill_tree:
+        if seq in sequence:
+            if flag < sequence.get(seq):
+                return 0
+            flag += 1
+    return 1
+            
+
+
 def solution(skill, skill_trees):
     answer = 0
+
+    sequence = {val: idx for idx, val in enumerate(skill)}
+
+    for skill_tree in skill_trees:
+        answer += check(answer, skill_tree, sequence)
+
     return answer
 
 
