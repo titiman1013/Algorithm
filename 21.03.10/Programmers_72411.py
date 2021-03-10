e5fc11ad1000840df00fa5320363a6a0dcd0e82e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-10 13:51:15 +0900

    21.03.10 problem1 clear

--- 21.03.10/Programmers_72411.py
+++ 21.03.10/Programmers_72411.py
@@ -1,5 +1,39 @@
+from itertools import combinations
+
+def check(orders, cnt, menu_dic):
+    for order in orders:
+        order = sorted(list(map(str, order)))
+        lst = list(combinations(order, cnt))
+        for val in lst:
+            tmp = "".join(val)
+            if menu_dic.get(tmp):
+                menu_dic[tmp] += 1
+            else:
+                menu_dic[tmp] = 1
+    return menu_dic
+
+
 def solution(orders, course):
     answer = []
+
+    for cnt in course:
+        menu_dic = {}
+        menu_dic = check(orders, cnt, menu_dic)
+
+        maxi = 0
+        menu = []
+        for key, val in menu_dic.items():
+            if val > 1:
+                if val > maxi:
+                    maxi = val
+                    menu = [key]
+                elif val == maxi:
+                    menu.append(key)
+        if menu:
+            answer += menu
+    
+    answer.sort()
+
     return answer
 
 
