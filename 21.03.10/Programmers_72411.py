e6afc6a609af9e924259a9697baefb82cadd6af3
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-10 11:34:44 +0900

    21.03.10 problem1

---
+++ 21.03.10/Programmers_72411.py
@@ -0,0 +1,15 @@
+def solution(orders, course):
+    answer = []
+    return answer
+
+
+
+
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
+print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]))
+print(solution(["XYZ", "XWY", "WXA"], [2, 3, 4]))
+
+# answer
+# ["AC", "ACDE", "BCFG", "CDE"]
+# ["ACD", "AD", "ADE", "CD", "XYZ"]
+# ["WX", "XY"]
