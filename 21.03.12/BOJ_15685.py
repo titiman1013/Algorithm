c87593e12ac616d7dd0800d58cf036e153cfa645
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-12 17:43:04 +0900

    21.03.12 problem1 clear

--- 21.03.12/BOJ_15685.py
+++ 21.03.12/BOJ_15685.py
@@ -1,5 +1,38 @@
 import sys; sys.stdin = open('15685.txt', 'r')
 
+def create(x, y, d):
+    if d == 0:
+        nx, ny = x + 1, y
+    elif d == 1:
+        nx, ny = x, y + 1
+    elif d == 2:
+        nx, ny = x - 1, y
+    else:
+        nx, ny = x, y - 1
+
+    tmp_lst = ((x, y), (nx, ny))
+    co = (nx, ny)
+
+    return tmp_lst, co
+
+
+def rotate(point_lst, co):
+    for point in point_lst:
+        pass
+
+
+def check():
+    pass
+
+
+
 for tc in range(1, int(input()) + 1):
     N = int(input())
     curves = [list(map(int, input().split())) for _ in range(N)]
+
+    curve_lst = []
+    for x, y, d, g in curves:
+        # 좌표리스트, 축(coordinate)
+        point_lst, co = create(x, y, d)
+        for _ in range(g):
+            point_lst += rotate(point_lst, co)
