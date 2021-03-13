ff9fe421b61bc3abbe007d7bf4238a51d525c8e8
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-13 14:07:50 +0900

    21.03.13 problem1

---
+++ 21.03.13/Programmers_12905.py
@@ -0,0 +1,13 @@
+def solution(board):
+    answer = 0
+    return answer
+
+
+
+
+print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
+print(solution([[0,0,1,1],[1,1,1,1]]))
+
+# answer
+# 9
+# 4
