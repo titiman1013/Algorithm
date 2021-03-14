d510a3d8a1520d4d42fb9b1d8cb3b1c945ad80b2
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-14 12:32:54 +0900

    21.03.14 problem1 clear

--- 21.03.14/Programmers_42895.py
+++ 21.03.14/Programmers_42895.py
@@ -1,6 +1,22 @@
 def solution(N, number):
-    answer = 0
-    return answer
+    # answer = 0
+
+    # 8번보다 많이 사용하는 경우는 생각 X
+    dp = [set([int(str(N) * i)]) for i in range(1, 9)]
+    for i in range(8):
+        for j in range(i):
+            for p in dp[j]:
+                for q in dp[i - j - 1]:
+                    dp[i].add(p * q)
+                    dp[i].add(p + q)
+                    dp[i].add(p - q)
+                    if q:
+                        dp[i].add(p // q)
+        print(dp[i])
+        if number in dp[i]:
+            return i + 1
+
+    return -1
 
 
 
