21a05dbf9d19be9c42ade27b69fea5f37b0329fa
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-15 10:35:36 +0900

    21.03.15 problem1 clear

--- 21.03.15/Programmers_12909.py
+++ 21.03.15/Programmers_12909.py
@@ -1,5 +1,18 @@
 def solution(s):
     answer = True
+    
+    que = []
+    for string in s:
+        if string == '(':
+            que.append(string)
+        elif string == ')':
+            if que:
+                que.pop()
+            else:
+                return False
+
+    if que: return False
+
     return answer
 
 
