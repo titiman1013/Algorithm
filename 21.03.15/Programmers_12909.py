31324edad1491d7c9cb28cdbfeb51f70513006fa
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-15 10:24:17 +0900

    21.03.15 problem1

---
+++ 21.03.15/Programmers_12909.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = True
+    return answer
+
+
+
+
+print(solution("()()"))
+print(solution("(())()"))
+print(solution(")()("))
+print(solution("(()("))
+
+# answer
+# true
+# true
+# false
+# flase
