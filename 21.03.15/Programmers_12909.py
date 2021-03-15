9ac2a1ec9df251f8ab00d0d12f9d0428123dcfcd
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-15 10:40:55 +0900

    21.03.15 problem1 add solve

--- 21.03.15/Programmers_12909.py
+++ 21.03.15/Programmers_12909.py
@@ -1,19 +1,37 @@
-def solution(s):
-    answer = True
+# def solution(s):
+#     answer = True
     
-    que = []
+#     que = []
+#     for string in s:
+#         if string == '(':
+#             que.append(string)
+#         elif string == ')':
+#             if que:
+#                 que.pop()
+#             else:
+#                 return False
+
+#     if que: return False
+
+#     return answer
+
+
+# 그냥 숫자로 판별 가능
+
+def solution(s):
+    answer = 0
+
     for string in s:
+        if answer < 0: return False
+        
         if string == '(':
-            que.append(string)
-        elif string == ')':
-            if que:
-                que.pop()
-            else:
-                return False
+            answer += 1
+        else:
+            answer -= 1
+    
+    if answer == 0: return True
+    else: return False
 
-    if que: return False
-
-    return answer
 
 
 
