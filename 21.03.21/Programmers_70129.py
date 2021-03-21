c95c24bc609adf5330c860814c9c15f850fc60a4
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-21 14:12:39 +0900

    21.03.21 problem1

---
+++ 21.03.21/Programmers_70129.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = []
+    return answer
+
+
+
+
+print(solution("110010101001"))
+print(solution("01110"))
+print(solution("1111111"))
+
+# answer
+# [3, 8]
+# [3, 3]
+# [4, 1]
