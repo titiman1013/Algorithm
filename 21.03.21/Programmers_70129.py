2f7d8fa5cc7c3341290de12860735b7efc0e3f6e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-21 14:30:53 +0900

    21.03.21 problem1 clear

--- 21.03.21/Programmers_70129.py
+++ 21.03.21/Programmers_70129.py
@@ -1,5 +1,15 @@
 def solution(s):
     answer = []
+
+    cnt = 0
+    zero_cnt = 0
+    while s != '1':
+        cnt += 1
+        zero_cnt += s.count('0')
+        s = str(format(s.count('1'), 'b'))
+
+    answer = [cnt, zero_cnt]
+
     return answer
 
 
