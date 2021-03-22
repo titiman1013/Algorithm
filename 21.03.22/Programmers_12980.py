3471f517b145207e72689712a4041cd39eefbebd
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-22 11:43:35 +0900

    21.03.22 problem1 add solve

--- 21.03.22/Programmers_12980.py
+++ 21.03.22/Programmers_12980.py
@@ -1,14 +1,16 @@
 def solution(n):
-    ans = 0
+    # ans = 0
     
-    while n != 1:
-        if n % 2:
-            ans += 1
-            n //= 2
-        else:
-            n //= 2
+    # while n != 1:
+    #     if n % 2:
+    #         ans += 1
+    #         n //= 2
+    #     else:
+    #         n //= 2
 
-    return ans + 1
+    # return ans + 1
+
+    return bin(n).count('1')
 
 
 
