4fc5e374335f0aaaaf245d608e23e7af8f4112ad
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-23 17:31:49 +0900

    21.03.23 problem1 clear

--- 21.03.23/Programmers_17676.py
+++ 21.03.23/Programmers_17676.py
@@ -1,6 +1,45 @@
+from datetime import datetime, timedelta
+
 def solution(lines):
     answer = 0
+
+    # print(True if "2016-09-15 01:00:04.001" < "2016-09-16 01:00:07.000" else False)
+    # print(True if datetime.strptime("2016-09-16 01:00:07.000", '%Y-%m-%d %H:%M:%S.%f') < datetime.strptime("2016-09-15 01:00:04.001", '%Y-%m-%d %H:%M:%S.%f') else False)
+    lines_list = [list(line.split()) for line in lines]
+    start_end_times = []
+    check_times = []
+    for D, S, T in lines_list:
+        # 끝에서 -1ms를 해준 값까지 포함되므로
+        mili_sec = int(round(float(T[:-1]), 3) * 1000 - 1)
+        end = datetime.strptime(D + ' ' + S, "%Y-%m-%d %H:%M:%S.%f")
+        start = end - timedelta(milliseconds=mili_sec)
+        # for문을 돌리기위한 (시작점, 끝점)을 리스트에 저장
+        start_end_times.append((start, end))
+        # 체크해야 할 시작점과 끝점 저장
+        check_times.append(start)
+        check_times.append(end)
+    
+    cnt_list = []
+    for i in range(len(check_times)):
+        traffic_start = check_times[i]
+        traffic_end = traffic_start + timedelta(milliseconds=999)
+        cnt = 0
+        for start, end in start_end_times:
+            # 끝점이 트래픽 시간에 포함
+            if start <= traffic_start and end >= traffic_start:
+                cnt += 1
+            # 시작점이 트래픽 시간에 포함
+            elif start <= traffic_end and end >= traffic_end:
+                cnt += 1
+            # 모든부분이 트래픽 시간에 포함
+            elif start >= traffic_start and end <= traffic_end:
+                cnt += 1
+        cnt_list.append(cnt)
+
+    answer = max(cnt_list)
+
     return answer
+
 
 
 
