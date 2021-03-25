c26e519a6e1e20568a47e8534b75212b0017e69f
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-25 15:59:28 +0900

    21.03.25 problem1 add best solve

--- 21.03.25/Programmers_68646.py
+++ 21.03.25/Programmers_68646.py
@@ -1,31 +1,49 @@
-import math
+# import math
+
+# def solution(a):
+#     answer = 0
+
+#     # 기본적으로 풍선은 큰 것을 터뜨린다
+#     # 앞, 뒤는 무조건 남길 수 있고, 
+#     # 가운데 위치한 풍선은 자신을 기준으로 좌리스트와 우리스트의 최소값보다 하나이상 작으면 남길 수 있다
+#     left, right = math.inf, math.inf
+#     # 양 끝단부터 최소값을 비교해서 결과값을 리스트에 저장
+#     arr = [[0] * len(a) for _ in range(2)]
+
+#     for i in range(len(a)):
+#         # 좌를 기준으로 작은 풍선을 남긴다
+#         if left > a[i]:
+#             left = a[i]
+#         arr[0][i] = left
+
+#         # 우를 기준으로 큰 풍선을 남긴다
+#         if right > a[-i - 1]:
+#             right = a[-i - 1]
+#         arr[1][-i - 1] = right
+
+#     # 최소값이 저장된 리스트를 기반으로 남길 수 있는지 확인
+#     for i in range(len(a)):
+#         if a[i] <= arr[0][i] or a[i] <= arr[1][i]:
+#             answer += 1
+
+#     return answer
+
+
+
+# best solve
 
 def solution(a):
-    answer = 0
-
-    # 기본적으로 풍선은 큰 것을 터뜨린다
-    # 앞, 뒤는 무조건 남길 수 있고, 
-    # 가운데 위치한 풍선은 자신을 기준으로 좌리스트와 우리스트의 최소값보다 하나이상 작으면 남길 수 있다
-    left, right = math.inf, math.inf
-    # 양 끝단부터 최소값을 비교해서 결과값을 리스트에 저장
-    arr = [[0] * len(a) for _ in range(2)]
-
-    for i in range(len(a)):
-        # 좌를 기준으로 작은 풍선을 남긴다
-        if left > a[i]:
-            left = a[i]
-        arr[0][i] = left
-
-        # 우를 기준으로 큰 풍선을 남긴다
-        if right > a[-i - 1]:
-            right = a[-i - 1]
-        arr[1][-i - 1] = right
-
-    # 최소값이 저장된 리스트를 기반으로 남길 수 있는지 확인
-    for i in range(len(a)):
-        if a[i] <= arr[0][i] or a[i] <= arr[1][i]:
-            answer += 1
-
+    answer = 1
+    M = min(a)
+    for _ in range(2):
+        m = a[0]
+        i = 1
+        while m != M:
+            if m >= a[i]:
+                m = a[i]
+                answer += 1
+            i += 1
+        a.reverse()
     return answer
 
 
