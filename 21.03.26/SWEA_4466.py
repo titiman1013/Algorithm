6ab6ca8002ea6e31a7e0b52cf1bba371b736dd15
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-26 16:27:22 +0900

    21.03.26 problem1 clear

--- 21.03.26/SWEA_4466.py
+++ 21.03.26/SWEA_4466.py
@@ -2,5 +2,10 @@ import sys; sys.stdin = open('4466.txt', 'r')
 
 for tc in range(1, int(input()) + 1):
     N, K = map(int, input().split())
-    score = list(map(int, input().split()))
-    
+    scores = list(map(int, input().split()))
+
+    answer = 0
+    scores.sort(reverse=True)
+    for score in scores[:K]:
+        answer += score
+    print(f'#{tc} {answer}')
