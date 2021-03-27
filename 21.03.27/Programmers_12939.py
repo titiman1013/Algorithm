72cc6b20876a6eb78c0bfb3db77d8113c8d2a77e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-27 10:08:16 +0900

    21.03.27 problem1

---
+++ 21.03.27/Programmers_12939.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = ''
+
+    return answer
+
+
+
+
+print(solution("1 2 3 4"))
+print(solution("-1 -2 -3 -4"))
+print(solution("-1 -1"))
+
+# answer
+# "1 4"
+# "-4 -1"
+# "-1 -1"
