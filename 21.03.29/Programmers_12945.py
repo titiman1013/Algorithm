788c3d8a32e1e9b01f920ae7553036cee8ed0c0b
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-29 14:30:55 +0900

    21.03.29 problem1 add dp solve

--- 21.03.29/Programmers_12945.py
+++ 21.03.29/Programmers_12945.py
@@ -1,8 +1,25 @@
+#
+
+# def solution(n):
+#     a, b = 0, 1
+#     for i in range(n):
+#         a, b = b, a + b
+#     return a
+
+
+
+#
+
 def solution(n):
-    a, b = 0, 1
-    for i in range(n):
-        a, b = b, a + b
-    return a
+    answer = 0
+
+    dp = [0, 1]
+    for i in range(2, n + 1):
+        dp.append(dp[i - 2] + dp[i - 1])
+
+    answer = dp[n] % 1234567
+
+    return answer
 
 
 
