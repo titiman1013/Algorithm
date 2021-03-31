5846bf283d5c44b63e58ebc99ad96984a78fccf5
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-03-31 10:16:23 +0900

    21.03.31 problem1

---
+++ 21.03.31/Programmers_12951.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = ''
+
+    return answer
+
+
+
+
+print(solution("3people unFollowed me"))
+print(solution("for the last week"))
+
+# answer
+# "3people Unfollowed Me"
+# "For The Last Week"
