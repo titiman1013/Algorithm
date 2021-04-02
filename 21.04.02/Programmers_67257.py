985b4ea7d1aa2928099fbc980908c6559b9de8eb
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-02 12:34:12 +0900

    21.04.02 problem1 add re and eval method

--- 21.04.02/Programmers_67257.py
+++ 21.04.02/Programmers_67257.py
@@ -42,6 +42,33 @@ def solution(expression):
 
 
 
+# 정규표현식 + eval function
+
+import re
+from itertools import permutations
+
+def solution(expression):
+    #1
+    op = [x for x in ['*','+','-'] if x in expression]
+    op = [list(y) for y in permutations(op)]
+    ex = re.split(r'(\D)',expression)
+
+    #2
+    a = []
+    for x in op:
+        _ex = ex[:]
+        for y in x:
+            while y in _ex:
+                tmp = _ex.index(y)
+                _ex[tmp-1] = str(eval(_ex[tmp-1]+_ex[tmp]+_ex[tmp+1]))
+                _ex = _ex[:tmp]+_ex[tmp+2:]
+        a.append(_ex[-1])
+
+    #3
+    return max(abs(int(x)) for x in a)
+
+
+
 
 
 
