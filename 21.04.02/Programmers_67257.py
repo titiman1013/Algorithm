485c4f2e1a524a25545e0e9a1800d927ea5e296f
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-02 12:15:46 +0900

    21.04.02 problem1 clear

--- 21.04.02/Programmers_67257.py
+++ 21.04.02/Programmers_67257.py
@@ -1,6 +1,43 @@
+from itertools import permutations
+
+def calc(expression_lst, order):
+    for x_th in order:
+        while x_th in expression_lst:
+            oper_idx = expression_lst.index(x_th)
+            a, b = expression_lst[oper_idx - 1], expression_lst[oper_idx + 1]
+            if x_th == '*':
+                tmp_calc = a * b
+            elif x_th == '+':
+                tmp_calc = a + b
+            elif x_th == '-':
+                tmp_calc = a - b
+            expression_lst = expression_lst[:oper_idx - 1] + [tmp_calc] + expression_lst[oper_idx + 2:]
+    return abs(expression_lst[0])
+
+
 def solution(expression):
     answer = 0
     
+    expression_lst = []
+    operators = []
+    tmp = ''
+    for string in expression:
+        if string == '+' or string == '-' or string == '*':
+            if string not in operators:
+                operators.append(string)
+            expression_lst.append(int(tmp))
+            tmp = ''
+            expression_lst.append(string)
+        if string.isnumeric():
+            tmp += string
+    expression_lst.append(int(tmp))
+
+    oper_order = list(permutations(operators, len(operators)))
+    for order in oper_order:
+        result = calc(expression_lst, order)
+        if result > answer:
+            answer = result
+
     return answer
 
 
