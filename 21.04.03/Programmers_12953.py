c57c583ceffd2b25a535366d69473222e784ae49
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-03 10:52:53 +0900

    21.04.03 problem1

---
+++ 21.04.03/Programmers_12953.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    answer = 0
+
+    return answer
+
+
+
+
+print(solution([2, 6, 8, 14]))
+print(solution([1, 2, 3]))
+print(solution([3, 4, 9, 16]))
+
+# answer
+# 168
+# 6
+# 144
