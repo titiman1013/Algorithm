f9daa863cfffb575cc6570787c3ece792e0b267d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-03 11:38:05 +0900

    21.04.03 problem1 false

--- 21.04.03/Programmers_12953.py
+++ 21.04.03/Programmers_12953.py
@@ -1,5 +1,39 @@
+# false
+
+# def solution(arr):
+#     answer = float('inf')
+
+#     for i in range(min(arr), 0, -1):
+#         cnt = 0
+#         for val in arr:
+#             if val % i == 0:
+#                 cnt += 1
+#         if cnt == len(arr):
+#             tmp = i
+#             for val in arr:
+#                 tmp *= val // i
+#             if tmp < answer:
+#                 answer = tmp
+
+#     return answer
+
+
+# false
+
 def solution(arr):
-    answer = 0
+    answer = float('inf')
+
+    results = []
+    for i in range(min(arr), 0, -1):
+        for val in arr:
+            tmp = i
+            if val % i == 0:
+                tmp *= val // i
+            else:
+                tmp *= val
+        results.append(tmp)
+            
+    answer = min(results)
 
     return answer
 
