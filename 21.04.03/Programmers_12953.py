f967b1109d566a2cebd415d19dc075c31e78f1d7
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-03 12:19:39 +0900

    21.04.03 problem1 clear

--- 21.04.03/Programmers_12953.py
+++ 21.04.03/Programmers_12953.py
@@ -40,33 +40,60 @@
 
 # false
 
-def calc(arr, i):
-    for idx, val in enumerate(arr):
-        if val % i == 0:
-            arr[idx] = val // i
+# def calc(arr, i):
+#     for idx, val in enumerate(arr):
+#         if val % i == 0:
+#             arr[idx] = val // i
 
-    result = i
-    for val in arr:
-        result *= val
+#     result = i
+#     for val in arr:
+#         result *= val
     
-    return result
+#     return result
+
+
+# def solution(arr):
+#     answer = float('inf')
+
+#     for i in range(2, max(arr) + 1):
+#         cnt = 0
+#         for val in arr:
+#             if val % i == 0:
+#                 cnt += 1
+#         if cnt >= 2:
+#             tmp = calc(arr, i)
+#             if tmp < answer:
+#                 answer = tmp
+    
+#     if answer == float('inf'):
+#         answer = calc(arr, 1)
+
+#     return answer
+
+
+# clear
+
+def GCD(num1, num2):
+    x, y = min(num1, num2), max(num1, num2)
+    while y:
+        x, y = y, x % y
+    return x
+
+
+def LCM(num1, num2):
+    x, y = min(num1, num2), max(num1, num2)
+    res = (x * y) // GCD(x, y)
+    return res
 
 
 def solution(arr):
-    answer = float('inf')
+    answer = 0
 
-    for i in range(2, max(arr) + 1):
-        cnt = 0
-        for val in arr:
-            if val % i == 0:
-                cnt += 1
-        if cnt >= 2:
-            tmp = calc(arr, i)
-            if tmp < answer:
-                answer = tmp
-    
-    if answer == float('inf'):
-        answer = calc(arr, 1)
+    while len(arr) > 1:
+        a, b = arr.pop(), arr.pop()
+        arr.append(LCM(a, b))
+
+    answer = arr[0]
 
     return answer
 
