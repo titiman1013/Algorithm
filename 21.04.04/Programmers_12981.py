b29fb83c680ab1aff1a5eb834a828026f0e8f19b
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-04 10:30:12 +0900

    21.04.04 problem1

---
+++ 21.04.04/Programmers_12981.py
@@ -0,0 +1,16 @@
+def solution(n, words):
+    answer = []
+
+    return answer
+
+
+
+
+print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
+print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
+print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
+
+# answer
+# [3, 3]
+# [0, 0]
+# [1, 3]
