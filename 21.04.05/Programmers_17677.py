05c81ad4675d2d6609b9c54381a5dd0ab64ad34f
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-05 10:16:44 +0900

    21.04.05 problem1

---
+++ 21.04.05/Programmers_17677.py
@@ -0,0 +1,18 @@
+def solution(str1, str2):
+    answer = 0
+
+    return answer
+
+
+
+
+print(solution('FRANCE', 'french'))
+print(solution('handshake', 'shake hands'))
+print(solution('aa1+aa2', 'AAAA12'))
+print(solution('E=M*C^2', 'e=m*c^2'))
+
+# answer
+# 16384
+# 65536
+# 43690
+# 65536
