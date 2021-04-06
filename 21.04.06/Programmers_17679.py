c3fb5e348d02530b3bef2db729c580a205da58a1
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-06 10:39:30 +0900

    21.04.06 problem1

---
+++ 21.04.06/Programmers_17679.py
@@ -0,0 +1,14 @@
+def solution(m, n, board):
+    answer = 0
+
+    return answer
+
+
+
+
+print(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"]))
+print(solution(6, 6, ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]))
+
+# answer
+# 14
+# 15
