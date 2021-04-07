85f00cabc4468124f6ccbc2870467ab776054892
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-07 11:25:50 +0900

    21.04.07 problem1 false

--- 21.04.07/Programmers_12985.py
+++ 21.04.07/Programmers_12985.py
@@ -1,6 +1,13 @@
-def solution(n, a, b):
-    answer = 0
+# false
 
+def solution(n,a,b):
+    answer = 1
+
+    while min(a, b) + 1 != max(a, b):
+        answer += 1
+        a = a // 2 + a % 2
+        b = b // 2 + b % 2
+        
     return answer
 
 
