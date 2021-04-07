8052abc3b55d0c01e267d93d919d894dd20413a8
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-07 11:42:18 +0900

    21.04.07 problem1 clear

--- 21.04.07/Programmers_12985.py
+++ 21.04.07/Programmers_12985.py
@@ -1,13 +1,27 @@
 # false
 
-def solution(n,a,b):
-    answer = 1
+# def solution(n,a,b):
+#     answer = 1
 
-    while min(a, b) + 1 != max(a, b):
-        answer += 1
-        a = a // 2 + a % 2
-        b = b // 2 + b % 2
+#     while min(a, b) + 1 != max(a, b):
+#         answer += 1
+#         a = a // 2 + a % 2
+#         b = b // 2 + b % 2
         
+#     return answer
+
+
+#
+
+def solution(n, a, b):
+    answer = 0
+
+    # 1, 2 의 경우의 수 차단을 위해 +1
+    while a != b:
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+        answer += 1
+
     return answer
 
 
