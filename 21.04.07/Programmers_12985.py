4d8eea34edad8181017835a76265302ee6ae7f55
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-07 11:48:07 +0900

    21.04.07 problem1 add binary search

--- 21.04.07/Programmers_12985.py
+++ 21.04.07/Programmers_12985.py
@@ -1,6 +1,6 @@
 # false
 
-# def solution(n,a,b):
+# def solution(n, a, b):
 #     answer = 1
 
 #     while min(a, b) + 1 != max(a, b):
@@ -13,14 +13,40 @@
 
 #
 
+# def solution(n, a, b):
+#     answer = 0
+
+#     # 1, 2 의 경우의 수 차단을 위해 +1
+#     while a != b:
+#         a = (a + 1) // 2
+#         b = (b + 1) // 2
+#         answer += 1
+
+#     return answer
+
+
+# binary search
+
+import math
+
 def solution(n, a, b):
     answer = 0
 
-    # 1, 2 의 경우의 수 차단을 위해 +1
-    while a != b:
-        a = (a + 1) // 2
-        b = (b + 1) // 2
-        answer += 1
+    a, b = min(a, b), max(a, b)
+    answer = int(math.log(n, 2))
+    s, e = 1, n
+    m = (s + e) // 2
+    while True:
+        if s <= m and e > m:
+            break
+        elif s >= m:
+            s = m
+            m = (s + e) // 2
+            answer -= 1
+        elif e <= m:
+            e = m
+            m = (s + e) // 2
+            answer -= 1
 
     return answer
 
