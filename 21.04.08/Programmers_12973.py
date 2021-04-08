9fabd23f0e859863af1717cf5b871bd1af28d0ad
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-08 11:06:49 +0900

    21.04.08 problem1 false

--- 21.04.08/Programmers_12973.py
+++ 21.04.08/Programmers_12973.py
@@ -1,5 +1,25 @@
+# false
+
 def solution(s):
     answer = 0
+
+    idx = 0
+    while len(s):
+        if len(s) == 1: break
+
+        if s[idx] == s[idx + 1]:
+            while s[idx] == s[idx + 1]:
+                if idx + 1 == len(s) - 1: 
+                    return 1
+                idx += 1
+            s = s[:idx - 1] + s[idx + 1:]
+            idx = 0
+            continue
+        
+        if idx + 1 == len(s) - 1:
+            break
+        
+        idx += 1
 
     return answer
 
