9cd0e90bd4ab88797cd371c2d7e1a1a7571a92fb
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-08 11:21:10 +0900

    21.04.08 problem1 clear

--- 21.04.08/Programmers_12973.py
+++ 21.04.08/Programmers_12973.py
@@ -1,25 +1,48 @@
 # false
 
+# def solution(s):
+#     answer = 0
+
+#     idx = 0
+#     while len(s):
+#         if len(s) == 1: break
+
+#         if s[idx] == s[idx + 1]:
+#             while s[idx] == s[idx + 1]:
+#                 if idx + 1 == len(s) - 1: 
+#                     return 1
+#                 idx += 1
+#             s = s[:idx - 1] + s[idx + 1:]
+#             idx = 0
+#             continue
+        
+#         if idx + 1 == len(s) - 1:
+#             break
+        
+#         idx += 1
+
+#     return answer
+
+
+#
+
 def solution(s):
     answer = 0
 
-    idx = 0
-    while len(s):
-        if len(s) == 1: break
+    stack = []
+    for string in s:
+        if stack:
+            if stack[-1] == string:
+                stack.pop()
+            else:
+                stack.append(string)
+        else:
+            stack.append(string)
 
-        if s[idx] == s[idx + 1]:
-            while s[idx] == s[idx + 1]:
-                if idx + 1 == len(s) - 1: 
-                    return 1
-                idx += 1
-            s = s[:idx - 1] + s[idx + 1:]
-            idx = 0
-            continue
-        
-        if idx + 1 == len(s) - 1:
-            break
-        
-        idx += 1
+    if stack:
+        answer = 0
+    else:
+        answer = 1
 
     return answer
 
