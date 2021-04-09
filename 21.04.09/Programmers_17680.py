0e5093d27608486bf2ac933c8e598c4c71a0664f
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-09 10:59:48 +0900

    21.04.09 problem1

---
+++ 21.04.09/Programmers_17680.py
@@ -0,0 +1,22 @@
+def solution(cacheSize, cities):
+    answer = 0
+
+    return answer
+
+
+
+
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
+print(solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
+print(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
+print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
+print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+
+# answer
+# 50
+# 21
+# 60
+# 52
+# 16
+# 25
