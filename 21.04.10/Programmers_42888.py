50ce0d8e2cb23759fa7228336b9af33c45a5ae37
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-10 10:58:04 +0900

    21.04.10 problem1 clear

--- 21.04.10/Programmers_42888.py
+++ 21.04.10/Programmers_42888.py
@@ -1,6 +1,21 @@
 def solution(record):
     answer = []
 
+    id_dict = dict()
+    for message in record:
+        log = list(map(str, message.split()))
+        if len(log) == 3:
+            behavior, id, nickname = log
+            if id_dict.get(id) != nickname:
+                id_dict[id] = nickname
+    
+    for message in record:
+        log = list(map(str, message.split()))
+        if log[0] == 'Enter':
+            answer.append(f'{id_dict[log[1]]}님이 들어왔습니다.')
+        elif log[0] == 'Leave':
+            answer.append(f'{id_dict[log[1]]}님이 나갔습니다.')
+
     return answer
 
 
