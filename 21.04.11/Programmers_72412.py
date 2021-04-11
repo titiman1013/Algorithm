9bc2c3c2a79ac1df747c512c98d2869e18202b5b
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-11 11:54:00 +0900

    21.04.11 problem1 add solves

--- 21.04.11/Programmers_72412.py
+++ 21.04.11/Programmers_72412.py
@@ -57,6 +57,126 @@ def solution(info, query):
 
 
 
+# 창완1
+
+import bisect
+
+def solution(info, query):
+    answer = []
+    index_dict = {'-':0,
+    'cpp': 1,
+    'java':2,
+    'python': 3,
+    'backend':1,
+    'frontend' :2,
+    'junior':1,
+    'senior':2,
+    'chicken':1,
+    'pizza':2,
+    }
+    store_query = [[] for _ in range(108)]
+    for info_string in info:
+        language,position,career,soulfood,score = info_string.split(' ')
+        score = int(score)
+        for ind1 in [0,index_dict[language]]:
+            for ind2 in [0,index_dict[position]]:
+                for ind3 in [0,index_dict[career]]:
+                    for ind4 in [0,index_dict[soulfood]]:
+                        ind = ind1*27 + ind2*9 + ind3*3 + ind4
+                        bisect.insort(store_query[ind],score)
+
+    for qu in query:
+        language,position,career,last_query = map(str.strip,qu.split('and'))
+        soulfood,score = last_query.split(' ')
+        score = int(score)
+        ind = index_dict[language]*27 + index_dict[position]*9 + index_dict[career]*3 + index_dict[soulfood]
+        cnt = len(store_query[ind]) - bisect.bisect_left(store_query[ind],score)
+        answer.append(cnt)
+    
+
+    return answer
+
+
+# 창완2
+
+from itertools import combinations
+
+def solution(info, query):
+    answer = []
+    languages = ['cpp','java','python','-']
+    positions = ['backend','frontend','-']
+    careers = ['junior','senior','-']
+    soulfoods = ['chicken','pizza','-']
+    total_dict = {language : {position : {career : {soulfood : [0]*100001 for soulfood in soulfoods} for career in careers}  for position in positions} for language in languages}
+    for info_string in info:
+        language,position,career,soulfood,score = info_string.split(' ')
+        score = int(score)
+        info_list = [language,position,career,soulfood]
+        for i in range(1<<4):
+            temp = []
+            for j in range(4):
+                if i&(1<<j):
+                    temp.append(info_list[j])
+                else:
+                    temp.append('-')
+            total_dict[temp[0]][temp[1]][temp[2]][temp[3]][score] += 1
+    for language in total_dict.keys():
+        for position in total_dict[language].keys():
+            for career in total_dict[language][position].keys():
+                for soulfood in total_dict[language][position][career].keys():
+                    for score in range(1,100001):
+                        total_dict[language][position][career][soulfood][score] += total_dict[language][position][career][soulfood][score-1]
+    for query_string in query:
+        language,position,career,last_query = map(str.strip,query_string.split('and'))
+        soulfood,score = last_query.split(' ')
+        score = int(score)
+        answer.append(total_dict[language][position][career][soulfood][100000] - total_dict[language][position][career][soulfood][score-1])
+    return answer
+
+
+# 창완3
+
+def solution(info, query):
+    answer = []
+    index_dict = {'-':0,
+    'cpp': 1,
+    'java':2,
+    'python': 3,
+    'backend':1,
+    'frontend' :2,
+    'junior':1,
+    'senior':2,
+    'chicken':1,
+    'pizza':2,
+    }
+    store_query = [[0]*100001 for _ in range(108)]
+    for info_string in info:
+        language,position,career,soulfood,score = info_string.split(' ')
+        score = int(score)
+        for ind1 in [0,index_dict[language]]:
+            for ind2 in [0,index_dict[position]]:
+                for ind3 in [0,index_dict[career]]:
+                    for ind4 in [0,index_dict[soulfood]]:
+                        ind = ind1*27 + ind2*9 + ind3*3 + ind4
+                        store_query[ind][score] += 1
+    for ind in range(108):
+        for score in range(1,100001):
+            store_query[ind][score] += store_query[ind][score-1]
+
+    for qu in query:
+        language,position,career,last_query = map(str.strip,qu.split('and'))
+        soulfood,score = last_query.split(' ')
+        score = int(score)
+        ind = index_dict[language]*27 + index_dict[position]*9 + index_dict[career]*3 + index_dict[soulfood]
+        answer.append(store_query[ind][100000] - store_query[ind][score-1])
+    
+
+    return answer
+
+
+
+
+
 
 print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
 
