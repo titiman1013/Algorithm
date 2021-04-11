c4abf3740a31a752261d7c166d5855e18cc17c53
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-11 11:46:04 +0900

    21.04.11 problem1 clear

--- 21.04.11/Programmers_72412.py
+++ 21.04.11/Programmers_72412.py
@@ -1,17 +1,56 @@
 # time over
 
+# def solution(info, query):
+#     answer = []
+
+#     for condition in query:
+#         lang, pos, career, food_score = map(str, condition.split(' and '))
+#         food, score = map(str, food_score.split())
+#         cnt = 0
+#         for resume in info:
+#             # applicant
+#             a_lang, a_pos, a_career, a_food, a_score = map(str, resume.split())
+#             if (lang == '-' or lang == a_lang) and (pos == '-' or pos == a_pos) and (career == '-' or career == a_career) and (food == '-' or food == a_food) and int(score) <= int(a_score):
+#                 cnt += 1
+#         answer.append(cnt)
+
+#     return answer
+
+
+#
+
 def solution(info, query):
     answer = []
 
+    resumes = dict()
+    # applicant
+    for val in info:
+        idx = len(val) - 1
+        for i in range(idx - 1, -1, -1):
+            if val[i].isnumeric():
+                idx -= 1
+            else:
+                break
+        person = val[:idx]
+        if resumes.get(int(val[idx:])):
+            resumes[int(val[idx:])].append(list(map(str, person.split())) + ['-'])
+        else:
+            resumes[int(val[idx:])] = [list(map(str, person.split())) + ['-']]
+    
+    score_lists = sorted(resumes, reverse=True)
+    
     for condition in query:
         lang, pos, career, food_score = map(str, condition.split(' and '))
         food, score = map(str, food_score.split())
         cnt = 0
-        for resume in info:
-            # applicant
-            a_lang, a_pos, a_career, a_food, a_score = map(str, resume.split())
-            if (lang == '-' or lang == a_lang) and (pos == '-' or pos == a_pos) and (career == '-' or career == a_career) and (food == '-' or food == a_food) and int(score) <= int(a_score):
-                cnt += 1
+        for score_list in score_lists:
+            if int(score) <= score_list:
+                needs = resumes.get(score_list)
+                for need in needs:
+                    if lang in need and pos in need and career in need and food in need:
+                        cnt += 1
+            else: break
+            
         answer.append(cnt)
 
     return answer
