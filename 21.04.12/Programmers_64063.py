999c55833202dfb96075179a2b3b704b7dfb322d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-12 11:13:26 +0900

    21.04.12 problem1 false

--- 21.04.12/Programmers_64063.py
+++ 21.04.12/Programmers_64063.py
@@ -1,7 +1,48 @@
+# false
+
+# def solution(k, room_number):
+#     answer = []
+
+#     room = [False] * (k + 1)
+#     for prefer_num in room_number:
+#         if room[prefer_num]:
+#             while room[prefer_num]:
+#                 prefer_num += 1
+#         answer.append(prefer_num)
+#         room[prefer_num] = True
+
+#     return answer
+
+
+# false
+
+# def solution(k, room_number):
+#     answer = []
+
+#     room = dict()
+#     for prefer_num in room_number:
+#         if room.get(prefer_num):
+#             while room.get(prefer_num):
+#                 prefer_num += 1
+#         answer.append(prefer_num)
+#         room[prefer_num] = True
+
+#     return answer
+
+
+# false
+
 def solution(k, room_number):
     answer = []
-
-    return answer
+    
+    room = []
+    for prefer_num in room_number:
+        if prefer_num in room:
+            while prefer_num in room:
+                prefer_num += 1
+        room.append(prefer_num)
+    
+    return room
 
 
 
