c009607d4a249bbcc460a41eddc0d889d1c87bad
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-13 10:29:16 +0900

    21.04.13 problem1

---
+++ 21.04.13/Programmers_77485.py
@@ -0,0 +1,16 @@
+def solution(rows, columns, queries):
+    answer = []
+
+    return answer
+
+
+
+
+print(solution(6, 6, [[2,2,5,4],[3,3,6,6],[5,1,6,3]]))
+print(solution(3, 3, [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]))
+print(solution(100, 97, [[1,1,100,97]]))
+
+# answer 
+# [8, 10, 25]
+# [1, 1, 5, 3]
+# [1]
