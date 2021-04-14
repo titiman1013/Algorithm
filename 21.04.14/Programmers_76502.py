e54f59b432eaea6c1b9a05caeeab0f4f7cbff51d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-14 10:55:07 +0900

    21.04.14 problem1 clear

--- 21.04.14/Programmers_76502.py
+++ 21.04.14/Programmers_76502.py
@@ -1,5 +1,35 @@
+def move(s, x):
+    return s[x:] + s[:x]
+
+
+def check(s):
+    stack = []
+    for string in s:
+        if string == '(' or string == '[' or string == '{':
+            stack.append(string)
+        else:
+            if stack:
+                if string == ')':
+                    if stack[-1] == '(': stack.pop()
+                    else: return False
+                elif string == ']':
+                    if stack[-1] == '[': stack.pop()
+                    else: return False
+                elif string == '}':
+                    if stack[-1] == '{': stack.pop()
+                    else: return False
+            else: return False
+    return False if stack else True
+
+
 def solution(s):
     answer = -1
+
+    answer = 0
+    for x in range(len(s)):
+        s_tmp = move(s, x)
+        if check(s_tmp):
+            answer += 1
 
     return answer
 
