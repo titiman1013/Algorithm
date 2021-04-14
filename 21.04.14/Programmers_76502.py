db46cd9d261f93be5de78ee4d5f4657462e148e5
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-14 10:10:49 +0900

    21.04.14 problem1

---
+++ 21.04.14/Programmers_76502.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = -1
+
+    return answer
+
+
+
+
+print(solution("[](){}"))
+print(solution("}]()[{"))
+print(solution("[)(]"))
+print(solution("}}}"))
+print(solution('((('))
+
+# answer
+# 3
+# 2
+# 0
+# 0
