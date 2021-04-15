fad3b5211f11c0835c607a6c890bf19606663c8d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-15 11:45:06 +0900

    21.04.15 problem1 false

--- 21.04.15/Programmers_42890.py
+++ 21.04.15/Programmers_42890.py
@@ -1,7 +1,26 @@
+# false
+
+from itertools import combinations
+from collections import Counter
+
 def solution(relation):
     answer = 0
 
+    N, M = len(relation), len(relation[0])
+
+    attributes = [i for i in range(M)]
+    for m in range(1, M + 1):
+        clusters = list(combinations(attributes, m))
+        for cluster in clusters:
+            check_lst = set()
+            for num in cluster:
+                tmp_lst = set()
+                for i in range(N):
+                    tmp_lst.add(relation[i][num])
+                check_lst.add(tmp_lst)
+            
     return answer
+    
 
 
 
