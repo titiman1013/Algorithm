3c7f46e1393b9fc8679e4e4beb3e8ee0e7f17a4f
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-17 10:19:34 +0900

    21.04.17 problem1

---
+++ 21.04.17/Programmers_67256.js
@@ -0,0 +1,17 @@
+function solution(numbers, hand) {
+  var answer = '';
+
+  return answer;
+}
+
+
+
+
+console.log(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"))
+console.log(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left"))
+console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right"))
+
+// answer
+// "LRLLLRLLRRL"
+// "LRLLRRLLLRR"
+// "LLRLLRLLRL"
