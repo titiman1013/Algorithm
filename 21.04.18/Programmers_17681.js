da8eebcb61cbfa2cde44a497cd0983b1956ad6d9
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-18 11:33:10 +0900

    21.04.18 problem1 clear

--- 21.04.18/Programmers_17681.js
+++ 21.04.18/Programmers_17681.js
@@ -1,42 +1,92 @@
 // false
 
+// function solution(n, arr1, arr2) {
+//     var answer = [];
+
+//     const room1 = [];
+//     for (let num1 of arr1) {
+//         let tmp1 = num1.toString(2);
+//         if (n !== tmp1.length) {
+//             tmp1 = '0' * (n - tmp1.length) + tmp1;
+//         }
+//         room1.push(tmp1);
+//     }
+
+//     const room2 = [];
+//     for (let num2 of arr2) {
+//         let tmp2 = num2.toString(2);
+//         if (n !== tmp2.length) {
+//             // console.log(tmp2.length)
+//             tmp2 = '0' * (n - tmp2.length) + tmp2;
+//         }
+//         room2.push(tmp2);
+//     }
+
+//     for (let i = 0; i < n; i++) {
+//         let line = '';
+//         for (let j = 0; j < n; j++) {
+//             if (room1[i][j] | room2[i][j]) {
+//                 line += '#';
+//             } else {
+//                 line += ' ';
+//             }
+//         }
+//         answer.push(line);
+//     }
+
+//     // console.log(room1, room2)
+
+//     return answer;
+// }
+
+
+// 
+
 function solution(n, arr1, arr2) {
-    var answer = [];
+  var answer = [];
 
-    const room1 = [];
-    for (let num1 of arr1) {
-        let tmp1 = num1.toString(2);
-        if (n !== tmp1.length) {
-            tmp1 = '0' * (n - tmp1.length) + tmp1;
-        }
-        room1.push(tmp1);
-    }
+  const room1 = [];
+  const room2 = [];
+  for (let idx = 0; idx < n; idx++) {
+      let binary1 = arr1[idx].toString(2);
+      let binary2 = arr2[idx].toString(2);
+      
+      // if (n !== binary1.length) {
+      //     let tmp1 = '';
+      //     for (let i = 0; i < n - binary1.length; i++) {
+      //         tmp1 += '0';
+      //     }
+      //     binary1 = tmp1 + binary1;
+      // }
+      binary1.padStart(n, '0');
 
-    const room2 = [];
-    for (let num2 of arr2) {
-        let tmp2 = num2.toString(2);
-        if (n !== tmp2.length) {
-            // console.log(tmp2.length)
-            tmp2 = '0' * (n - tmp2.length) + tmp2;
-        }
-        room2.push(tmp2);
-    }
+      // if (n !== binary2.length) {
+      //     let tmp2 = ''
+      //     for (let i = 0; i < n - binary2.length; i++) {
+      //         tmp2 += '0';
+      //     }
+      //     binary2 = tmp2 + binary2;
+      // }
+      binary2.padStart(n, '0');
 
-    for (let i = 0; i < n; i++) {
-        let line = '';
-        for (let j = 0; j < n; j++) {
-            if (room1[i][j] | room2[i][j]) {
-                line += '#';
-            } else {
-                line += ' ';
-            }
-        }
-        answer.push(line);
-    }
+      room1.push(binary1);
+      room2.push(binary2);
+      console.log(binary1, binary2)
+  }
 
-    // console.log(room1, room2)
+  for (let i = 0; i < n; i++) {
+      let line = '';
+      for (let j = 0; j < n; j++) {
+          if (room1[i][j] | room2[i][j]) {
+              line += '#';
+          } else {
+              line += ' ';
+          }
+      }
+      answer.push(line);
+  }
 
-    return answer;
+  return answer;
 }
 
 
