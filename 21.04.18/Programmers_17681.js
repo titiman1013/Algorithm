73bb5c4caeaec8d3b4dc8077181d06f4bb1ee034
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-18 10:51:39 +0900

    21.04.18 problem1 false

--- 21.04.18/Programmers_17681.js
+++ 21.04.18/Programmers_17681.js
@@ -1,7 +1,42 @@
-function solution(n, arr1, arr2) {
-  var answer = [];
+// false
 
-  return answer;
+function solution(n, arr1, arr2) {
+    var answer = [];
+
+    const room1 = [];
+    for (let num1 of arr1) {
+        let tmp1 = num1.toString(2);
+        if (n !== tmp1.length) {
+            tmp1 = '0' * (n - tmp1.length) + tmp1;
+        }
+        room1.push(tmp1);
+    }
+
+    const room2 = [];
+    for (let num2 of arr2) {
+        let tmp2 = num2.toString(2);
+        if (n !== tmp2.length) {
+            // console.log(tmp2.length)
+            tmp2 = '0' * (n - tmp2.length) + tmp2;
+        }
+        room2.push(tmp2);
+    }
+
+    for (let i = 0; i < n; i++) {
+        let line = '';
+        for (let j = 0; j < n; j++) {
+            if (room1[i][j] | room2[i][j]) {
+                line += '#';
+            } else {
+                line += ' ';
+            }
+        }
+        answer.push(line);
+    }
+
+    // console.log(room1, room2)
+
+    return answer;
 }
 
 
