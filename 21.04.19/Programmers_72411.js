4448294566707fe3e3f211de311bd2155e759a4d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-19 10:27:55 +0900

    21.04.19 problem1

---
+++ 21.04.19/Programmers_72411.js
@@ -0,0 +1,17 @@
+function solution(orders, course) {
+  var answer = [];
+
+  return answer;
+}
+
+
+
+
+console.log(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
+console.log(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]))
+console.log(solution(["XYZ", "XWY", "WXA"], [2,3,4]))
+
+// answer
+// ["AC", "ACDE", "BCFG", "CDE"]
+// ["ACD", "AD", "ADE", "CD", "XYZ"]
+// ["WX", "XY"]
