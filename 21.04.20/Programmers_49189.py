a1704eba8e3b3ca8a4fc06b70b1a7a6b5aaad667
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-20 11:32:57 +0900

    21.04.20 problem1 clear

--- 21.04.20/Programmers_49189.py
+++ 21.04.20/Programmers_49189.py
@@ -1,5 +1,29 @@
+#
+
+from collections import deque
+
 def solution(n, vertex):
     answer = 0
+
+    graph = dict()
+    for node1, node2 in vertex:
+        graph.setdefault(node1, []).append(node2)
+        graph.setdefault(node2, []).append(node1)
+    
+    # node number, depth
+    deq = deque([[1, 0]])
+    # 거리 및 방문 여부
+    visited = [-1] * (n + 1)
+    while deq:
+        n, depth = deq.popleft()
+        visited[n] = depth
+        for i in graph[n]:
+            if visited[i] == -1:
+                visited[i] = 0
+                deq.append([i, depth + 1])
+        depth += 1
+    
+    answer = visited.count(max(visited))
 
     return answer
 
