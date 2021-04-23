f5b4d352b734690b0f7b1c61699bf3841fb477ec
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-23 10:30:26 +0900

    21.04.23 problem1 clear

--- 21.04.23/SWEA_1234.py
+++ 21.04.23/SWEA_1234.py
@@ -3,3 +3,14 @@ import sys; sys.stdin = open('1234.txt', 'r')
 for tc in range(1, 11):
     N, arr = map(str, input().split())
     arr = list(map(str, arr))
+    
+    while True:
+        for i in range(len(arr) - 1):
+            if arr[i] == arr[i + 1]:
+                del arr[i + 1]
+                del arr[i]
+                break
+        else:
+            break
+    
+    print(f'#{tc} {"".join(arr)}')
