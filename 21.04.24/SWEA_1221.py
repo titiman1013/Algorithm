badd1eb7a55b5ede773110c04b6f30a0073b1121
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-24 10:34:10 +0900

    21.04.24 problem1 clear

--- 21.04.24/SWEA_1221.py
+++ 21.04.24/SWEA_1221.py
@@ -1,5 +1,17 @@
 import sys; sys.stdin = open('1221.txt', 'r')
 
+from collections import Counter
+
 for tc in range(1, int(input()) + 1):
     test_case, N = map(str, input().split())
     arr = list(input().split())
+    
+    arr_counter = Counter(arr)
+    seqs = ['ZRO', 'ONE', 'TWO', 'THR', 'FOR', 'FIV', 'SIX', 'SVN', 'EGT', 'NIN']
+    
+    print(test_case)
+    for seq in seqs:
+        if arr_counter.get(seq):
+            for _ in range(arr_counter.get(seq)):
+                print(seq, end=' ')
+    print()
