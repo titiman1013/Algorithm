4c371ab66f9a43fec79916c057c5e75845de3c70
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-25 11:03:04 +0900

    21.04.25 problem1 clear

--- 21.04.25/SWEA_4676.py
+++ 21.04.25/SWEA_4676.py
@@ -1,7 +1,21 @@
 import sys; sys.stdin = open('4676.txt', 'r')
 
+from collections import Counter
+
 for tc in range(1, int(input()) + 1):
     strings = input()
     H = int(input())
     positions = list(map(int, input().split()))
     answer = ''
+
+    pos_dict = Counter(positions)
+    for idx, string in enumerate(strings):
+        if pos_dict.get(idx):
+            for _ in range(pos_dict.get(idx)):
+                answer += '-'
+        answer += string
+    if pos_dict.get(len(strings)):
+        for _ in range(pos_dict.get(len(strings))):
+            answer += '-'
+
+    print(f'#{tc} {answer}')
