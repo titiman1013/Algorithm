062f1eaf742141d462a89e900ec229bb3b4ef486
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-26 10:56:14 +0900

    21.04.26 problem1 clear

--- 21.04.26/SWEA_10912.py
+++ 21.04.26/SWEA_10912.py
@@ -1,5 +1,20 @@
 import sys; sys.stdin = open('10912.txt', 'r')
 
+from collections import Counter
+
 for tc in range(1, int(input()) + 1):
     strings = input()
     answer = ''
+
+    result = []
+    str_counter = Counter(strings)
+    for string, cnt in str_counter.items():
+        if cnt % 2:
+            result.append(string)
+    
+    if result:
+        answer = "".join(sorted(result))
+    else:
+        answer = 'Good'
+
+    print(f'#{tc} {answer}')
