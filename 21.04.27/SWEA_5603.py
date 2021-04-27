30c7bbb8c1f3888b52d0e0d6f83ea3f9ea3dfd3e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-27 10:28:49 +0900

    21.04.27 problem1 clear

--- 21.04.27/SWEA_5603.py
+++ 21.04.27/SWEA_5603.py
@@ -4,3 +4,11 @@ for tc in range(1, int(input()) + 1):
     N = int(input())
     dummys = list(int(input()) for _ in range(N))
     answer = 0
+
+    avg = sum(dummys) // N
+    tmp = 0
+    for dummy in dummys:
+        tmp += abs(dummy - avg)
+    answer = tmp // 2
+
+    print(f'#{tc} {answer}')
