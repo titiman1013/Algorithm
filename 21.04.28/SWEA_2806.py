9f400f73b1a2b7040888901c2a5bf52e089868d1
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-29 10:26:26 +0900

    21.04.29 problem1

--- 21.04.28/SWEA_2806.py
+++ 21.04.28/SWEA_2806.py
@@ -1,5 +1,30 @@
 import sys; sys.stdin = open('2806.txt', 'r')
 
+# array, row, column 
+def backtrack(arr, idx, pos):
+    global answer, N
+
+    if idx == N:
+        answer += 1
+        return
+    
+    for j in range(N):
+        for up in range(idx - 1, -1, -1):
+            if arr[up] == j: continue
+            elif abs(arr[up] - j) == abs(up - idx): continue
+            else:
+                arr[idx] = j
+                backtrack(arr, idx + 1, 0)
+
+
+
 for tc in range(1, int(input()) + 1):
     N = int(input())
     answer = 0
+
+    arr = list(-1 for _ in range(N))
+    for j in range(N):
+        arr[0] = j
+        backtrack(arr, 1, j)
+
+    print(f'#{tc} {answer}')
---
+++ 21.04.29/SWEA_11856.py
@@ -0,0 +1,4 @@
+import sys; sys.stdin = open('11856.txt', 'r')
+
+for tc in range(1, int(input()) + 1):
+    S = input()
