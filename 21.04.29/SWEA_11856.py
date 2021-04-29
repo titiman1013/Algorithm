a3ecb93ad679ce34775c918ae9b837b90e74ba98
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-04-29 10:36:53 +0900

    21.04.29 problem1 clear

--- 21.04.29/SWEA_11856.py
+++ 21.04.29/SWEA_11856.py
@@ -1,4 +1,15 @@
 import sys; sys.stdin = open('11856.txt', 'r')
 
+from collections import Counter
+
 for tc in range(1, int(input()) + 1):
     S = input()
+    
+    print(f'#{tc}', end=" ")
+    counter = Counter(S)
+    for key, val in counter.items():
+        if val != 2:
+            print('No')
+            break
+    else:
+        print('Yes')
