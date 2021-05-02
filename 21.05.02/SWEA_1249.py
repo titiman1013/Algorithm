d4bcf4c273d418f55aa1e5e63f48783c8d41df9f
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-02 10:41:07 +0900

    21.05.02 problem1 false

--- 21.05.02/SWEA_1249.py
+++ 21.05.02/SWEA_1249.py
@@ -1,5 +1,41 @@
 import sys; sys.stdin = open('1249.txt', 'r')
 
+from collections import deque
+
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+def bfs(arr, visited, S, G):
+    deq = deque([S])
+    while deq:
+        x, y = deq.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < N and 0 <= ny < N:
+                # if [nx, ny] == G:
+                #     if visited[nx][ny]:
+                #         if visited[nx][ny] > visited[x][y]:
+                #             visited[nx][ny] = visited[x][y]
+                #     else:
+                #         visited[nx][ny] = visited[x][y]
+                if visited[nx][ny] == 0:
+                    visited[nx][ny] = visited[x][y] + arr[nx][ny]
+                    deq.append((nx, ny))
+                else:
+                    if visited[nx][ny] > visited[x][y] + arr[nx][ny]:
+                        visited[nx][ny] = visited[x][y] + arr[nx][ny]
+                        deq.append((nx, ny))
+
+
+
 for tc in range(1, int(input()) + 1):
     N = int(input())
     arr = list(list(map(int, input())) for _ in range(N))
+
+    visited = [[0] * N for _ in range(N)]
+    S, G = [0, 0], [N - 1, N - 1]
+    
+    bfs(arr, visited, S, G)
+    answer = visited[G[0]][G[1]]
+
+    print(f'#{tc} {answer}')
