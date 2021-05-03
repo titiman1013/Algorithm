e164d97ae4ca142a86af54fe6eba9b2000d9a0e7
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-03 11:23:47 +0900

    21.05.03 problem1 clear

--- 21.05.03/SWEA_1486.py
+++ 21.05.03/SWEA_1486.py
@@ -1,5 +1,16 @@
 import sys; sys.stdin = open('1486.txt', 'r')
 
+from itertools import combinations
+
 for tc in range(1, int(input()) + 1):
     N, B = map(int, input().split())
     H = list(map(int, input().split()))
+
+    answer = 10000
+    for i in range(1, N + 1):
+        clusters = list(combinations(H, i))
+        for cluster in clusters:
+            if sum(cluster) >= B and sum(cluster) - B < answer:
+                answer = sum(cluster) - B
+    
+    print(f'#{tc} {answer}')
