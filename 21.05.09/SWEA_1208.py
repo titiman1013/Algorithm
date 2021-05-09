c3755189dd00a5161aaf6f9d9653b3669bc1cb7b
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-09 10:47:43 +0900

    21.05.09 problem1 clear

--- 21.05.09/SWEA_1208.py
+++ 21.05.09/SWEA_1208.py
@@ -3,3 +3,12 @@ import sys; sys.stdin = open('1208.txt', 'r')
 for tc in range(1, 11):
     N = int(input())
     arr = list(map(int, input().split()))
+
+    for i in range(N):
+        if arr.count(max(arr)) >= len(arr) - 1: break
+        arr[arr.index(max(arr))] -= 1
+        arr[arr.index(min(arr))] += 1
+
+    answer = max(arr) - min(arr)
+
+    print(f'#{tc} {answer}')
