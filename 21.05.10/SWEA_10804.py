822e1944eca794fa0a84f3ef6876bedecdb93d47
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-10 10:23:02 +0900

    21.05.10 problem1 clear

--- 21.05.10/SWEA_10804.py
+++ 21.05.10/SWEA_10804.py
@@ -2,3 +2,18 @@ import sys; sys.stdin = open('10804.txt', 'r')
 
 for tc in range(1, int(input()) + 1):
     strings = input()
+    answer = []
+
+    for string in strings:
+        if string == 'b':
+            answer.append('d')
+        elif string == 'd':
+            answer.append('b')
+        elif string == 'p':
+            answer.append('q')
+        elif string == 'q':
+            answer.append('p')
+    
+    answer = "".join(reversed(answer))
+
+    print(f'#{tc} {answer}')
