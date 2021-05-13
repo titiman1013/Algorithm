fe5fe063ab4146d3c1cd37d0d8a8fcd0d78bfb01
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-13 10:43:04 +0900

    21.05.13 problem1 clear

--- 21.05.13/SWEA_11315.py
+++ 21.05.13/SWEA_11315.py
@@ -1,5 +1,29 @@
 import sys; sys.stdin = open('11315.txt', 'r')
 
+dx, dy = [-1, 1, 0, 0, -1, -1, 1, 1], [0, 0, -1, 1, -1, 1, -1, 1]
+
+def search(board, N):
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 'o':
+                for k in range(8):
+                    cnt = 1
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    while 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 'o':
+                        cnt += 1
+                        if cnt >= 5:
+                            return 'YES'
+                        nx += dx[k]
+                        ny += dy[k]
+    return 'NO'
+
+
+
 for tc in range(1, int(input()) + 1):
     N = int(input())
     board = [input() for _ in range(N)]
+
+    answer = search(board, N)
+
+    print(f'#{tc} {answer}')
