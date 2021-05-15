73be2ee8b6280f8f65867ad6a5ef5daf4790ab43
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-15 14:58:43 +0900

    21.05.15 problem1 reculsive error

--- 21.05.15/Programmers_43164.py
+++ 21.05.15/Programmers_43164.py
@@ -1,5 +1,40 @@
+# reculsive error
+
+from collections import defaultdict
+
+def reculsive(depart, visited):
+    global answer
+    if len(visited) == N + 1:
+        if answer:
+            for i in range(N + 1):
+                if visited[i] < answer[i]:
+                    answer = visited
+                    return
+            else:
+                return
+            
+        else:
+            answer = visited
+            return
+
+    for arrive in airports[depart]:
+        visited.append(arrive)
+        reculsive(arrive, visited)
+
+
+
+
 def solution(tickets):
+    global answer, airports, N
     answer = []
+
+    N = len(tickets)
+    airports = defaultdict(list)
+    for depart, arrive in tickets:
+        airports[depart].append(arrive)
+
+    reculsive("ICN", ["ICN"])
+
     return answer
 
 
