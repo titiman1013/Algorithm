778718e8b6530c4fdbc750651f1c0b5f712acf16
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-15 15:32:02 +0900

    21.05.15 problem1 clear

--- 21.05.15/Programmers_43164.py
+++ 21.05.15/Programmers_43164.py
@@ -1,41 +1,67 @@
 # reculsive error
 
+# from collections import defaultdict
+
+# def reculsive(depart, visited):
+#     global answer
+#     if len(visited) == N + 1:
+#         if answer:
+#             for i in range(N + 1):
+#                 if visited[i] < answer[i]:
+#                     answer = visited
+#                     return
+#             else:
+#                 return
+            
+#         else:
+#             answer = visited
+#             return
+
+#     for arrive in airports[depart]:
+#         visited.append(arrive)
+#         reculsive(arrive, visited)
+
+
+
+
+# def solution(tickets):
+#     global answer, airports, N
+#     answer = []
+
+#     N = len(tickets)
+#     airports = defaultdict(list)
+#     for depart, arrive in tickets:
+#         airports[depart].append(arrive)
+
+#     reculsive("ICN", ["ICN"])
+
+#     return answer
+
+
+
+
+#
+
 from collections import defaultdict
 
-def reculsive(depart, visited):
+def dfs(start, airports):
     global answer
-    if len(visited) == N + 1:
-        if answer:
-            for i in range(N + 1):
-                if visited[i] < answer[i]:
-                    answer = visited
-                    return
-            else:
-                return
-            
-        else:
-            answer = visited
-            return
-
-    for arrive in airports[depart]:
-        visited.append(arrive)
-        reculsive(arrive, visited)
-
-
+    while airports[start]:
+        end = airports[start].pop()
+        dfs(end, airports)
+    answer.append(start)
 
 
 def solution(tickets):
-    global answer, airports, N
+    global answer
     answer = []
 
-    N = len(tickets)
     airports = defaultdict(list)
-    for depart, arrive in tickets:
+    for depart, arrive in sorted(tickets, reverse=True):
         airports[depart].append(arrive)
+    dfs("ICN", airports)
 
-    reculsive("ICN", ["ICN"])
-
-    return answer
+    return answer[::-1]
 
 
 
