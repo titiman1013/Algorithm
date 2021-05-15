31215bf5e3762804d0d07668f874adca63563e0d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-15 14:07:14 +0900

    21.05.15 problem1

---
+++ 21.05.15/Programmers_43164.py
@@ -0,0 +1,13 @@
+def solution(tickets):
+    answer = []
+    return answer
+
+
+
+
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
+
+# answer
+# ["ICN", "JFK", "HND", "IAD"]
+# ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
