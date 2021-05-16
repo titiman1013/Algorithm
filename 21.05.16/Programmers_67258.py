15217ae7a19675f08f1c2b22cf43d41f19c39746
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-16 20:58:53 +0900

    21.05.16 problem1 clear

--- 21.05.16/Programmers_67258.py
+++ 21.05.16/Programmers_67258.py
@@ -1,28 +1,63 @@
 # false
 
+# from collections import Counter
+
+# def solution(gems):
+#     answer = []
+
+#     gems_counter = Counter(gems)
+#     N = len(gems_counter)
+#     if N == 1:
+#         answer = [1, 1]
+#     elif N == len(gems):
+#         answer = [1, len(gems)]
+#     else:
+#         s, e = 0, len(gems)
+#         while s <= e:
+#             if len(Counter(gems[s:e - 1])) == N:
+#                 e -= 1
+#             elif len(Counter(gems[s + 1:e])) == N:
+#                 s += 1
+#             else:
+#                 answer = [s + 1, e]
+#                 break
+
+#     return answer
+
+
+
+#
+
 from collections import Counter
 
 def solution(gems):
     answer = []
 
-    gems_counter = Counter(gems)
-    N = len(gems_counter)
+    M, N = len(gems), len(Counter(gems))
     if N == 1:
         answer = [1, 1]
-    elif N == len(gems):
-        answer = [1, len(gems)]
+    elif N == M:
+        answer = [1, M]
     else:
-        s, e = 0, len(gems)
+        s, e = 0, 0
+        check = {gems[0]: 1}
+        answer = [1, M]
         while s <= e:
-            if len(Counter(gems[s:e - 1])) == N:
-                e -= 1
-            elif len(Counter(gems[s + 1:e])) == N:
+            if len(check) == N:
+                if e - s < answer[1] - answer[0]:
+                    answer = [s + 1, e + 1]
+                if check[gems[s]] == 1:
+                    del check[gems[s]]
+                else:
+                    check[gems[s]] -= 1
                 s += 1
-            else:
-                answer = [s + 1, e]
-                break
+            elif len(check) < N:
+                e += 1
+                if e == M: break
+                check[gems[e]] = check.get(gems[e], 0) + 1
 
     return answer
+
 
 
 
