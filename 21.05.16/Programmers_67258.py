9837e6ffbe56359558f4e34fbead84dd17e36c2a
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-16 19:52:14 +0900

    21.05.16 problem1

---
+++ 21.05.16/Programmers_67258.py
@@ -0,0 +1,18 @@
+def solution(gems):
+    answer = []
+    return answer
+
+
+
+
+print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
+print(solution(["AA", "AB", "AC", "AA", "AC"]))
+print(solution(["XYZ", "XYZ", "XYZ"]))
+print(solution(["ZZZ", "YYY", "NNNN", "YYY", "BBB"]))
+print(solution(['E', 'F', 'A', 'F', 'F', 'B', 'A', 'E', 'E', 'E']))
+
+# answer
+# [3, 7]
+# [1, 3]
+# [1, 1]
+# [1, 5]
