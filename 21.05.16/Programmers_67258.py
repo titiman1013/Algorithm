24dc0f9920a0dcaf80a43eacdc3cec997280d845
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-16 20:24:37 +0900

    21.05.16 problem1 false

--- 21.05.16/Programmers_67258.py
+++ 21.05.16/Programmers_67258.py
@@ -1,5 +1,27 @@
+# false
+
+from collections import Counter
+
 def solution(gems):
     answer = []
+
+    gems_counter = Counter(gems)
+    N = len(gems_counter)
+    if N == 1:
+        answer = [1, 1]
+    elif N == len(gems):
+        answer = [1, len(gems)]
+    else:
+        s, e = 0, len(gems)
+        while s <= e:
+            if len(Counter(gems[s:e - 1])) == N:
+                e -= 1
+            elif len(Counter(gems[s + 1:e])) == N:
+                s += 1
+            else:
+                answer = [s + 1, e]
+                break
+
     return answer
 
 
