5b0ec9c75740c000838987b1209398329cd0d8b8
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-19 11:10:59 +0900

    21.05.19 problem1 clear

--- 21.05.19/BOJ_1912.py
+++ 21.05.19/BOJ_1912.py
@@ -3,3 +3,9 @@ import sys; sys.stdin = open('1912.txt', 'r')
 for tc in range(1, int(input()) + 1):
     N = int(input())
     nums = list(map(int, input().split()))
+    
+    dp = [val for val in nums]
+    for i in range(1, N):
+        dp[i] = max(dp[i], dp[i - 1] + nums[i])
+    
+    print(max(dp))
