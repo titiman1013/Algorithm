580bf0cc70d3133672a9bafb3783764b6e8ea538
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-21 11:17:36 +0900

    21.05.21 problem1 clear

--- 21.05.21/BOJ_11052.py
+++ 21.05.21/BOJ_11052.py
@@ -3,3 +3,20 @@ import sys; sys.stdin = open('11052.txt', 'r')
 for tc in range(1, int(input()) + 1):
     N = int(input())
     cards = [0] + list(map(int, input().split()))
+
+    dp = [0 for _ in range(N + 1)]
+    for cnt in range(1, N + 1):
+        for k in range(1, cnt + 1):
+            dp[cnt] = max(dp[cnt], dp[cnt - k] + cards[k])
+    
+    print(max(dp))
+
+
+
+# answer
+# 10
+# 50
+# 55
+# 50
+# 20
+# 18
