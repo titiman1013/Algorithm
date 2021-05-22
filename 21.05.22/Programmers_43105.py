2901a19d24db49fe9dd7dd32c2b97d2d85aa8ff7
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-22 22:09:05 +0900

    21.05.22 problem1 clear

--- 21.05.22/Programmers_43105.py
+++ 21.05.22/Programmers_43105.py
@@ -1,5 +1,21 @@
 def solution(triangle):
     answer = 0
+
+    N = len(triangle)
+    dp = [[0] * n for n in range(1, N + 1)]
+    dp[0][0] = triangle[0][0]
+
+    for i in range(1, N):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                dp[i][j] = dp[i - 1][0] + triangle[i][j]
+            elif j == len(triangle[i]) - 1:
+                dp[i][j] = dp[i - 1][-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j]
+
+    answer = max(dp[-1])
+
     return answer
 
 
