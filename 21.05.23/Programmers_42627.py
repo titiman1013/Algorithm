07e1bec9ed8fddca7b0eaf52a8bca72364979943
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-24 00:32:11 +0900

    21.05.23 problem1 clear

--- 21.05.23/Programmers_42627.py
+++ 21.05.23/Programmers_42627.py
@@ -1,5 +1,26 @@
+import heapq
+
 def solution(jobs):
     answer = 0
+
+    N = len(jobs)
+    heap = []
+    time, last, cnt = 0, -1, 0
+    while cnt < N:
+        for start, during in jobs:
+            if last < start <= time:
+                heapq.heappush(heap, (during, start))
+        if len(heap) > 0:
+            during, start = heapq.heappop(heap)
+            last = time
+            time += during
+            answer += (time - start)
+            cnt += 1
+        else:
+            time += 1
+
+    answer //= N
+
     return answer
 
 
