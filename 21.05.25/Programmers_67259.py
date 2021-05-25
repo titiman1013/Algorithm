14c3659276d0de68ed447dd295f0bda21554a55e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-25 14:39:13 +0900

    21.05.25 problem1 clear

--- 21.05.25/Programmers_67259.py
+++ 21.05.25/Programmers_67259.py
@@ -1,5 +1,31 @@
+from collections import deque
+
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
 def solution(board):
     answer = 0
+
+    N = len(board)
+    start, end = (0, 0), (N - 1, N - 1)
+    costs = [[float('inf')] * N for _ in range(N)]
+
+    deq = deque()
+    deq.append((0, 0, 0, 1))
+    deq.append((0, 0, 0, 3))
+    costs[0][0] = 0
+    while deq:
+        x, y, cost, dir = deq.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            tmp_cost = cost + 100 if dir == k else cost + 600
+            if 0 <= nx < N and 0 <= ny < N:
+                if board[nx][ny] == 0 and tmp_cost <= costs[nx][ny]:
+                    costs[nx][ny] = tmp_cost
+                    deq.append((nx, ny, tmp_cost, k))
+
+    answer = costs[end[0]][end[1]]
+
     return answer
 
 
