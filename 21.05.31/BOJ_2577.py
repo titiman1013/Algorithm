e50e10eeb67e00a6dcecd7ae80db6ed04bf1ba5d
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-05-31 10:25:16 +0900

    21.05.31 problem1 clear

--- 21.05.31/BOJ_2577.py
+++ 21.05.31/BOJ_2577.py
@@ -1,5 +1,14 @@
 import sys; sys.stdin = open('2577.txt', 'r')
 
+from collections import Counter
+
 A = int(input())
 B = int(input())
 C = int(input())
+
+answer = Counter(str(A * B * C))
+for i in range(10):
+    if answer.get(str(i)):
+        print(answer.get(str(i)))
+    else:
+        print(0)
