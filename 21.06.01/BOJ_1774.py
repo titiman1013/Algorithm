e04cb302df2c2d48e670a2de4a0e3f18d0448f6e
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-01 14:46:03 +0900

    21.06.01 problem1 add kruskal solve

--- 21.06.01/BOJ_1774.py
+++ 21.06.01/BOJ_1774.py
@@ -50,39 +50,93 @@ import sys; sys.stdin = open('1774.txt', 'r')
 
 # Prim faster
 
-import heapq
+# import heapq
+
+# N, M = map(int, input().split())
+
+# node = [list(map(int, input().split())) for _ in range(N)]
+# visited = [False] * N
+# graph = [[0] * N for _ in range(N)]
+
+# for i in range(N):
+#     for j in range(i+1, N):
+#         dis = (node[i][0] - node[j][0]) ** 2 + (node[i][1] - node[j][1]) ** 2
+#         dis = dis ** 0.5
+#         graph[i][j] = dis
+#         graph[j][i] = dis
+
+# for i in range(M):
+#     a, b = map(int, input().split())
+#     graph[a - 1][b - 1] = 0
+#     graph[b - 1][a - 1] = 0
+
+
+# heap = [[0, 0]]
+# answer = 0
+# for i in range(N):
+#     while heap:
+#         cost, idx = heapq.heappop(heap)
+#         if visited[idx]:
+#             continue
+#         for i in range(N):
+#             if not visited[i]:
+#                 heapq.heappush(heap, [graph[idx][i], i])
+#         visited[idx] = True
+#         answer += cost
+#         break
+
+# print(f'{answer:.2f}')
+
+
+
+
+# Kruskal
+
+import sys, heapq
+
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] == x: return x
+
+    parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(x, y):
+    x, y = find(x), find(y)
+
+    if x == y: return False
+
+    if rank[x] < rank[y]:
+        x, y = y, x
+
+    parent[y] = x
+
+    if rank[x] == rank[y]:
+        rank[x] += 1
+    return True
+
+
 
 N, M = map(int, input().split())
+points = [0] + [list(map(int, input().split())) for _ in range(N)]
+parent = [i for i in range(N + 1)]
+rank = [1] * (N + 1)
+for _ in range(M):
+    union(*map(int, input().split()))
 
-node = [list(map(int, input().split())) for _ in range(N)]
-visited = [False] * N
-graph = [[0] * N for _ in range(N)]
+heap = []
+for i in range(1, N):
+    for j in range(i + 1, N + 1):
+        dis = (abs(points[i][0] - points[j][0]) ** 2 + abs(points[i][1] - points[j][1]) ** 2) ** 0.5
+        heapq.heappush(heap, (dis, i, j))
 
-for i in range(N):
-    for j in range(i+1, N):
-        dis = (node[i][0] - node[j][0]) ** 2 + (node[i][1] - node[j][1]) ** 2
-        dis = dis ** 0.5
-        graph[i][j] = dis
-        graph[j][i] = dis
-
-for i in range(M):
-    a, b = map(int, input().split())
-    graph[a - 1][b - 1] = 0
-    graph[b - 1][a - 1] = 0
-
-
-heap = [[0, 0]]
 answer = 0
-for i in range(N):
-    while heap:
-        cost, idx = heapq.heappop(heap)
-        if visited[idx]:
-            continue
-        for i in range(N):
-            if not visited[i]:
-                heapq.heappush(heap, [graph[idx][i], i])
-        visited[idx] = True
+while heap:
+    cost, start, end = heapq.heappop(heap)
+
+    if union(start, end):
         answer += cost
-        break
 
 print(f'{answer:.2f}')
