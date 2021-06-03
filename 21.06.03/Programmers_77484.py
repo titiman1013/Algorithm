27548770d893aec61cd824507bae378bd428491a
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-03 11:10:54 +0900

    21.06.03 problem1 clear

--- 21.06.03/Programmers_77484.py
+++ 21.06.03/Programmers_77484.py
@@ -1,7 +1,26 @@
 def solution(lottos, win_nums):
     answer = []
 
+    win_a_lottery = {
+        0: 6,
+        1: 6,
+        2: 5,
+        3: 4,
+        4: 3,
+        5: 2,
+        6: 1,
+    }
 
+    max_num = 0
+    min_num = 0
+
+    for lotto in lottos:
+        if lotto in win_nums:
+            min_num += 1
+        elif lotto == 0:
+            max_num += 1
+
+    answer = [win_a_lottery.get(min_num + max_num), win_a_lottery.get(min_num)]
 
     return answer
 
