9bcd42e9770995828c12ca8bd48d142b1e658240
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-03 10:36:35 +0900

    21.06.03 problem1

---
+++ 21.06.03/Programmers_77484.py
@@ -0,0 +1,18 @@
+def solution(lottos, win_nums):
+    answer = []
+
+
+
+    return answer
+
+
+
+
+print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
+print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]))
+print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]))
+
+# answer
+# [3, 5]
+# [1, 6]
+# [1, 1]
