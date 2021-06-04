9fd4dbb0f6877071a1cd917ddc782efe5f4f6e5f
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-04 11:01:59 +0900

    21.06.04 problem1 clear

--- 21.06.04/Programmers_42628.py
+++ 21.06.04/Programmers_42628.py
@@ -1,7 +1,28 @@
+import heapq
+
 def solution(operations):
     answer = []
 
+    heap = []
+    for operation in operations:
+        command, num = operation.split()
+        if command == 'I':
+            heapq.heappush(heap, int(num))
+        elif command == 'D':
+            if heap:
+                if num == '1':
+                    reverse_heap = [-val for val in heap]
+                    heapq.heapify(reverse_heap)
+                    heapq.heappop(reverse_heap)
+                    heap = [-val for val in reverse_heap]
+                    heapq.heapify(heap)
+                elif num == '-1':
+                    heapq.heappop(heap)
 
+    if heap:
+        answer = [max(heap), min(heap)]
+    else:
+        answer = [0, 0]
 
     return answer
 
