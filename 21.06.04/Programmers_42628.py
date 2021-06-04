1e496622885819df92aad74e0193e32bd7a103c7
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-04 10:24:46 +0900

    21.06.04 problem1

---
+++ 21.06.04/Programmers_42628.py
@@ -0,0 +1,16 @@
+def solution(operations):
+    answer = []
+
+
+
+    return answer
+
+
+
+
+print(solution(["I 16","D 1"]))
+print(solution(["I 7","I 5","I -5","D -1"]))
+
+# answer
+# [0, 0]
+# [7, 5]
