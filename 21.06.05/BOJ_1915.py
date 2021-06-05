5682457173bfa0eb842207a5a70f560d240dc3df
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-05 17:54:11 +0900

    21.06.05 problem1 false

--- 21.06.05/BOJ_1915.py
+++ 21.06.05/BOJ_1915.py
@@ -6,3 +6,23 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 arr = [list(map(int, input())) for _ in range(N)]
+
+answer = 0
+for i in range(N):
+    for j in range(M):
+        if arr[i][j]:
+            cnt = 1
+            flag = True
+            for p in range(min(N - i, M - j)):
+                if flag == False: break
+                for q in range(min(N - i, M - j)):
+                    print((i, j), (p, q), (i + p, j + q), cnt)
+                    if arr[i + p][j + q] == 0:
+                        flag = False
+                        break
+                else: 
+                    cnt += 1
+            if cnt ** 2 > answer and flag:
+                answer = cnt ** 2
+
+print(answer)
