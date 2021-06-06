7ceafca117ce72373a58cb26660f9c98aefed0c4
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-06 22:55:13 +0900

    21.06.06 problem1 clear

--- 21.06.06/BOJ_2493.py
+++ 21.06.06/BOJ_2493.py
@@ -6,3 +6,18 @@ input = sys.stdin.readline
 
 N = int(input().strip('\n'))
 heights = list(map(int, input().split()))
+
+answer = []
+stack = []
+for idx, height in enumerate(heights):
+    while stack and heights[stack[-1]] < height:
+        stack.pop()
+
+    if stack:
+        answer.append(stack[-1] + 1)
+    else:
+        answer.append(0)
+
+    stack.append(idx)
+
+print(*answer)
