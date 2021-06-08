6476e5bb95e44bb83832efd50d2739c96b27a633
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-08 11:03:27 +0900

    21.06.08 problem1 clear

--- 21.06.08/Programmers_72412.py
+++ 21.06.08/Programmers_72412.py
@@ -1,7 +1,53 @@
+from collections import defaultdict
+import bisect
+
 def solution(info, query):
     answer = []
 
+    case_nums = {
+        '-': 0,
+        'cpp': 1,
+        'java': 2,
+        'python': 3,
+        'backend': 1,
+        'frontend': 2,
+        'junior': 1,
+        'senior': 2,
+        'chicken': 1,
+        'pizza': 2,
+    }
 
+    # all_case = defaultdict(dict)
+    all_case = defaultdict(list)
+    for applicant in info:
+        language, task, career, food, score = map(str, applicant.split())
+        
+        for l_n in [0, case_nums[language]]:
+            for t_n in [0, case_nums[task]]:
+                for c_n in [0, case_nums[career]]:
+                    for f_n in [0, case_nums[food]]:
+                        key_num = str(l_n) + str(t_n) + str(c_n) + str(f_n)
+                        # all_case[key_num][int(score)] = all_case[key_num].get(int(score), 0) + 1
+                        all_case[key_num].append(int(score))
+
+    for case in all_case.keys():
+        all_case[case].sort()
+    
+    for requirements in query:
+        requirement = list(map(str, requirements.split()))
+        
+        # total = 0
+        score_list = all_case.get(str(case_nums[requirement[0]]) + str(case_nums[requirement[2]]) + str(case_nums[requirement[4]]) + str(case_nums[requirement[6]]))
+        required_score = int(requirement[7])
+        if score_list:
+            # for score, cnt in score_list.items():
+            #     if score >= required_score:
+            #         total += cnt
+            answer.append(len(score_list) - bisect.bisect_left(score_list, required_score))
+
+        # answer.append(total)
+        else:
+            answer.append(0)
 
     return answer
 
