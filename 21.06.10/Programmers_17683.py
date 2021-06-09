66e779c6079984a5733ed02e1c94247319d5e8d6
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-09 10:16:46 +0900

    21.06.09 problem1

---
+++ 21.06.10/Programmers_17683.py
@@ -0,0 +1,21 @@
+def solution(m, musicinfos):
+    answer = ''
+
+
+
+    return answer
+
+
+
+
+
+
+print(solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
+print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+print(solution("AAA", ["12:00,12:03,1,AAA", "12:00,12:04,2,AAA", "12:00,12:03,3,AAA"]))
+
+# answer
+# "HELLO"
+# "FOO"
+# "WORLD"
