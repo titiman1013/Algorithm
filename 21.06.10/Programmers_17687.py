185f8631738b27ca14430c81b0eb9fed59f1548b
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-10 10:11:20 +0900

    21.06.10 problem1

---
+++ 21.06.10/Programmers_17687.py
@@ -0,0 +1,16 @@
+def solution(n, t, m, p):
+    answer = ''
+
+    return answer
+
+
+
+
+print(solution(2, 4, 2, 1))
+print(solution(16, 16, 2, 1))
+print(solution(16, 16, 2, 2))
+
+# answer
+# "0111"
+# "02468ACE11111111"
+# "13579BDF01234567"
