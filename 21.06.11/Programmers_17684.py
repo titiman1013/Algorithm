dc10973294506dd0ebb03936412a4e8664bdafc3
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-11 10:32:01 +0900

    21.06.11 problem1

---
+++ 21.06.11/Programmers_17684.py
@@ -0,0 +1,16 @@
+def solution(msg):
+    answer = []
+    return answer
+
+
+
+
+
+print(solution('KAKAO'))
+print(solution('TOBEORNOTTOBEORTOBEORNOT'))
+print(solution('ABABABABABABABAB'))
+
+# answer
+# [11, 1, 27, 15]
+# [20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34]
+# [1, 2, 27, 29, 28, 31, 30]
