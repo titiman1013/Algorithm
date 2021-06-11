cd05eb7d6e3d4465e92e4435b076d87ea4c8fb8b
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-11 12:17:53 +0900

    21.06.11 problem1 clear

--- 21.06.11/Programmers_17684.py
+++ 21.06.11/Programmers_17684.py
@@ -1,35 +1,61 @@
 # give up
 
+# def solution(msg):
+#     answer = []
+
+#     dictionary = {chr(i): i - 64 for i in range(65, 91)}
+#     idx = 0
+#     numbering = 27
+#     w, c = msg[0], msg[1]
+#     while idx < len(msg):
+#         answer.append(dictionary.get(w))
+        
+#         # end = 1
+#         # print(w, dictionary.get(msg[idx:idx + end]))
+#         # while dictionary.get(msg[idx:idx + end]) == None:
+#         #     end += 1
+#         # print(msg[idx:idx + end + 1])
+#         # dictionary[msg[idx:idx + end + 1]] = numbering
+#         # numbering += 1
+
+#         # idx += end
+#         # w, c = c, msg[idx + 1]
+
+#         dictionary[w + c] = numbering
+#         numbering += 1
+
+#         idx += len(c)
+#         cnt = 1
+#         while dictionary.get(msg[idx:idx + cnt]):
+#             if idx + cnt < len(msg):
+#                 cnt += 1
+        
+
+#     return answer
+
+
+
+#
+
 def solution(msg):
     answer = []
 
     dictionary = {chr(i): i - 64 for i in range(65, 91)}
-    idx = 0
+    stack = list(map(str, msg[::-1]))
     numbering = 27
-    w, c = msg[0], msg[1]
-    while idx < len(msg):
-        answer.append(dictionary.get(w))
-        
-        # end = 1
-        # print(w, dictionary.get(msg[idx:idx + end]))
-        # while dictionary.get(msg[idx:idx + end]) == None:
-        #     end += 1
-        # print(msg[idx:idx + end + 1])
-        # dictionary[msg[idx:idx + end + 1]] = numbering
-        # numbering += 1
 
-        # idx += end
-        # w, c = c, msg[idx + 1]
-
-        dictionary[w + c] = numbering
-        numbering += 1
-
-        idx += len(c)
-        cnt = 1
-        while dictionary.get(msg[idx:idx + cnt]):
-            if idx + cnt < len(msg):
-                cnt += 1
-        
+    while stack:
+        w = stack.pop()
+        if stack:
+            while dictionary.get(w + stack[-1]):
+                w += stack.pop()
+                if len(stack) == 0: break
+            answer.append(dictionary.get(w))
+            if stack:
+                dictionary[w + stack[-1]] = numbering
+            numbering += 1
+        else:
+            answer.append(dictionary.get(w))
 
     return answer
 
