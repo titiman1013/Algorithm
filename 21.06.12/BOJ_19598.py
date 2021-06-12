5209cc441cc8f182cbc9562d30dd36913cf83fc2
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-12 23:09:28 +0900

    21.06.12 problem1 time exceed

--- 21.06.12/BOJ_19598.py
+++ 21.06.12/BOJ_19598.py
@@ -1,9 +1,27 @@
 import sys; sys.stdin = open('19598.txt', 'r')
 
-import sys
+# time exceed
+
+import sys, heapq
 
 input = sys.stdin.readline
 
 for tc in range(1, int(input()) + 1):
     N = int(input())
     conferences = [tuple(map(int, input().split())) for _ in range(N)]
+
+    conferences = sorted(conferences, key=lambda x : x[0])
+
+    answer = 0
+    heap = []
+    for start, end in conferences:
+        if heap:
+            if min(heap) <= start:
+                heapq.heappop(heap)
+            heapq.heappush(heap, end)
+            if len(heap) > answer:
+                answer = len(heap)
+        else:
+            heapq.heappush(heap, end)
+
+    print(answer)
