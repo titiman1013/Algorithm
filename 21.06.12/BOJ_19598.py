049b2dd4570c057cdefe27c9299e61826af09167
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-12 22:40:45 +0900

    21.06.12 problem1

---
+++ 21.06.12/BOJ_19598.py
@@ -0,0 +1,9 @@
+import sys; sys.stdin = open('19598.txt', 'r')
+
+import sys
+
+input = sys.stdin.readline
+
+for tc in range(1, int(input()) + 1):
+    N = int(input())
+    conferences = [tuple(map(int, input().split())) for _ in range(N)]
