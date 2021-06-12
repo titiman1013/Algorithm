d1b50479bede52a1ae04f4d4a888a1f990fed141
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-12 23:39:24 +0900

    21.06.12 problem1 add python

--- 21.06.12/BOJ_19598.py
+++ 21.06.12/BOJ_19598.py
@@ -31,26 +31,52 @@ import sys; sys.stdin = open('19598.txt', 'r')
 
 # pypy
 
+# import sys, heapq
+
+# input = sys.stdin.readline
+
+# for tc in range(1, int(input()) + 1):
+#     N = int(input())
+#     heap = []
+#     for _ in range(N):
+#         heapq.heappush(heap, tuple(map(int, input().split())))
+
+#     answer = 0
+#     rooms = []
+#     while heap:
+#         start, end = heapq.heappop(heap)
+#         if rooms:
+#             if min(rooms) <= start:
+#                 rooms[rooms.index(min(rooms))] = end
+#                 continue
+#         rooms.append(end)
+#         if len(rooms) > answer:
+#             answer = len(rooms)
+
+#     print(answer)
+
+
+
+#
+
 import sys, heapq
 
 input = sys.stdin.readline
 
 for tc in range(1, int(input()) + 1):
     N = int(input())
-    heap = []
-    for _ in range(N):
-        heapq.heappush(heap, tuple(map(int, input().split())))
+    conferences = [tuple(map(int, input().split())) for _ in range(N)]
+
+    conferences.sort()
 
     answer = 0
-    rooms = []
-    while heap:
-        start, end = heapq.heappop(heap)
-        if rooms:
-            if min(rooms) <= start:
-                rooms[rooms.index(min(rooms))] = end
-                continue
-        rooms.append(end)
-        if len(rooms) > answer:
-            answer = len(rooms)
+    heap = []
+    for start, end in conferences:
+        if (not heap or heap[0] > start):
+            answer += 1
+            heapq.heappush(heap, end)
+        else:
+            heapq.heappop(heap)
+            heapq.heappush(heap, end)
 
     print(answer)
