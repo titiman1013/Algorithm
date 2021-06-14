b5c5599259acb263a6bd9fac8483eca7ac24113a
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-14 11:16:49 +0900

    21.06.13 problem1 clear

--- 21.06.14/BOJ_11000.py
+++ 21.06.14/BOJ_11000.py
@@ -1,8 +1,22 @@
 import sys; sys.stdin = open('11000.txt', 'r')
 
-import sys
+import sys, heapq
 
 input = sys.stdin.readline
 
 N = int(input())
 lectures = [tuple(map(int, input().split())) for _ in range(N)]
+
+lectures.sort()
+
+answer = 0
+heap = []
+for start, end in lectures:
+    if (not heap or heap[0] > start):
+        answer += 1
+        heapq.heappush(heap, end)
+    else:
+        heapq.heappop(heap)
+        heapq.heappush(heap, end)
+
+print(answer)
