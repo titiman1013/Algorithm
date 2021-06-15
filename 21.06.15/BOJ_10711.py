9ac9d9a5a97e11b85424c15b147d6dc45b1bc1c6
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-15 11:37:12 +0900

    21.06.15 problem1 time error

--- 21.06.15/BOJ_10711.py
+++ 21.06.15/BOJ_10711.py
@@ -2,59 +2,108 @@ import sys; sys.stdin = open('10711.txt', 'r')
 
 # time error
 
+# import sys
+
+# input = sys.stdin.readline
+# dx, dy = [-1, -1, 0, 1, 1, 1, 0, -1], [0, 1, 1, 1, 0, -1, -1, -1]
+
+# def search_sand(H, W):
+#     arr = [[0] * W for _ in range(H)]
+
+#     for i in range(H):
+#         for j in range(W):
+#             if board[i][j].isnumeric():
+#                 arr[i][j] = int(board[i][j])
+#     return arr
+
+
+# def wave(H, W):
+#     corrosion = False
+#     arr = [[0] * W for _ in range(H)]
+    
+#     for x in range(H):
+#         for y in range(W):
+#             if board[x][y]:
+#                 sand_cnt = board[x][y]
+#                 water_cnt = 0
+#                 for k in range(8):
+#                     nx = x + dx[k]
+#                     ny = y + dy[k]
+#                     if 0 <= nx < H and 0 <= ny < W:
+#                         if board[nx][ny] == 0:
+#                             water_cnt += 1
+#                             if sand_cnt - water_cnt == 0: break
+#                 else:
+#                     arr[x][y] = sand_cnt
+#                     continue
+ 
+#                 arr[x][y] = 0
+#                 corrosion = True
+
+#     return corrosion, arr
+ 
+
+
+# for tc in range(1, int(input()) + 1):
+#     H, W = map(int, input().split())
+#     board = [list(map(str, input())) for _ in range(H)]
+
+#     board = search_sand(H, W)
+
+#     answer = 0
+#     while True:
+#         flag, board = wave(H, W)
+#         if flag:
+#             answer += 1
+#         else: break
+
+#     print(answer)
+
+
+
+
+
+# time error
+
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 dx, dy = [-1, -1, 0, 1, 1, 1, 0, -1], [0, 1, 1, 1, 0, -1, -1, -1]
 
-def search_sand(H, W):
-    arr = [[0] * W for _ in range(H)]
-
+def search_waters(H, W):
+    waters = []
     for i in range(H):
         for j in range(W):
             if board[i][j].isnumeric():
-                arr[i][j] = int(board[i][j])
-    return arr
+                board[i][j] = int(board[i][j])
+            else:
+                board[i][j] = 0
+                waters.append((i, j))
+                
+    return waters
 
-
-def wave(H, W):
-    corrosion = False
-    arr = [[0] * W for _ in range(H)]
-    
-    for x in range(H):
-        for y in range(W):
-            if board[x][y]:
-                sand_cnt = board[x][y]
-                water_cnt = 0
-                for k in range(8):
-                    nx = x + dx[k]
-                    ny = y + dy[k]
-                    if 0 <= nx < H and 0 <= ny < W:
-                        if board[nx][ny] == 0:
-                            water_cnt += 1
-                            if sand_cnt - water_cnt == 0: break
-                else:
-                    arr[x][y] = sand_cnt
-                    continue
- 
-                arr[x][y] = 0
-                corrosion = True
-
-    return corrosion, arr
- 
 
 
 for tc in range(1, int(input()) + 1):
     H, W = map(int, input().split())
     board = [list(map(str, input())) for _ in range(H)]
 
-    board = search_sand(H, W)
+    waters = search_waters(H, W)
 
     answer = 0
-    while True:
-        flag, board = wave(H, W)
-        if flag:
-            answer += 1
-        else: break
+    while waters:
+        tmp_waters = []
+        answer += 1
+        for x, y in waters:
+            for k in range(8):
+                nx = x + dx[k]
+                ny = y + dy[k]
+                if 0 <= nx < H and 0 <= ny < W:
+                    if board[nx][ny] > 0:
+                        board[nx][ny] -= 1
+                        if board[nx][ny] == 0:
+                            tmp_waters.append((nx, ny))
+        waters = tmp_waters
 
-    print(answer)
+    print(answer - 1)
