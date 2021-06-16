3bd841f226c8fcbf3eb07ba9479c6768eba12581
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-16 10:41:41 +0900

    21.06.16 problem1 clear

--- 21.06.16/BOJ_14567.py
+++ 21.06.16/BOJ_14567.py
@@ -7,3 +7,13 @@ input = sys.stdin.readline
 for tc in range(1, int(input()) + 1):
     N, M = map(int, input().split())
     prerequisites = [tuple(map(int, input().split())) for _ in range(M)]
+
+    prerequisites.sort()
+
+    degrees = [1] * (N + 1)
+    for prerequisite, subject in prerequisites:
+        degrees[subject] = max(degrees[subject], degrees[prerequisite] + 1)
+
+    for i in range(1, N + 1):
+        print(degrees[i], end=' ')
+    print()
