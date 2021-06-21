ef257441d821a86832a2a71711522088dc5b6dcf
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-21 10:49:54 +0900

    21.06.21 problem1 false

--- 21.06.21/BOJ_1005.py
+++ 21.06.21/BOJ_1005.py
@@ -1,8 +1,40 @@
 import sys; sys.stdin = open('1005.txt', 'r')
 
+# false
+
 import sys
+from collections import deque
 
 input = sys.stdin.readline
+
+def topo_sort():
+    deq = deque()
+    output = []
+    for i in range(1, N + 1):
+        if degrees[i] == 0:
+            deq.append(i)
+            output.append(i)
+
+    answer = 0
+    while deq:
+        max_time = 0
+        tmp_deq = deque()
+        for current in deq:
+            if current == target: 
+                answer += times[current]
+                return answer
+            for next in graphs.get(current):
+                degrees[next] -= 1
+                max_time = max(max_time, times[current])
+                if degrees[next] == 0:
+                    tmp_deq.append(next)
+                    output.append(next)
+        deq = tmp_deq
+        answer += max_time
+    
+    return answer
+
+
 
 for _ in range(1, int(input()) + 1):
     for tc in range(1, int(input()) + 1):
@@ -10,3 +42,11 @@ for _ in range(1, int(input()) + 1):
         times = [0] + list(map(int, input().split()))
         orders = [tuple(map(int, input().split())) for _ in range(K)]
         target = int(input())
+
+        graphs = {i: [] for i in range(N + 1)}
+        degrees = [0 for _ in range(N + 1)]
+        for pre, post in orders:
+            graphs[pre].append(post)
+            degrees[post] += 1
+
+        print(topo_sort())
