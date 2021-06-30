1bb356b2b270e58c9dbec19f11a163df19611dea
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-06-30 10:21:23 +0900

    21.06.30 problem1

---
+++ 21.06.30/BOJ_16236.py
@@ -0,0 +1,10 @@
+import sys; sys.stdin = open('16236.txt', 'r')
+
+import sys
+
+
+input = sys.stdin.readline
+
+for tc in range(1, int(input()) + 1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
