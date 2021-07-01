3ed3bfcc40a95e4ae8de8a76dd4daaf9874a24ae
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-07-01 10:29:50 +0900

    21.07.01 problem1

---
+++ 21.07.01/Programmers_17686.py
@@ -0,0 +1,14 @@
+def solution(files):
+    answer = []
+    return answer
+
+
+
+
+print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
+print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
+print(solution(["img000012345", "img1.png","img2","IMG02"]))
+
+# answer
+# ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
+# ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
