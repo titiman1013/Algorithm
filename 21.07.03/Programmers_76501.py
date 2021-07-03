b17d6d64fd9691f10fe27ae3feef9b3418dec759
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-07-03 10:14:18 +0900

    21.07.03 problem1

---
+++ 21.07.03/Programmers_76501.py
@@ -0,0 +1,13 @@
+def solution(absolutes, signs):
+    answer = 123456789
+    return answer
+
+
+
+
+print(solution([4,7,12], [True, False, True]))
+print(solution([1,2,3], [False, False, True]))
+
+# answer
+# 9
+# 0
