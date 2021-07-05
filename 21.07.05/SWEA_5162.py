be696af3115e3fef12ea6f72e57a0328590029d1
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-07-05 10:49:16 +0900

    21.07.05 problem1 clear

--- 21.07.05/SWEA_5162.py
+++ 21.07.05/SWEA_5162.py
@@ -2,3 +2,7 @@ import sys; sys.stdin = open('5162.txt', 'r')
 
 for tc in range(1, int(input()) + 1):
     cost_A, cost_B, total = map(int, input().split())
+
+    answer = max(total // min(cost_A, cost_B), (total // (cost_A + cost_B) * 2))
+    
+    print(f'#{tc} {answer}')
