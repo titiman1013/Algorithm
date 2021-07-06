8a67e36931c42422779a4c90859b665ee468255b
hyunsuk Rho <hyunsukr1013@gmail.com>
2021-07-06 10:37:29 +0900

    21.07.06 problem1 clear

--- 21.07.06/SWEA_4047.py
+++ 21.07.06/SWEA_4047.py
@@ -1,5 +1,26 @@
 import sys; sys.stdin = open('4047.txt', 'r')
 
 
+patterns = {
+    'S': 0,
+    'D': 1,
+    'H': 2,
+    'C': 3,
+}
+
 for tc in range(1, int(input()) + 1):
     cards_info = input()
+
+    answer = [13, 13, 13, 13]
+    card_list = dict()
+    for i in range(0, len(cards_info), 3):
+        card_info = cards_info[i:i + 3]
+        if card_list.get(card_info):
+            answer = 'ERROR'
+            break
+        else:
+            card_list[card_info] = 1
+            answer[patterns.get(card_info[0])] -= 1
+    
+    print(f'#{tc}', end=" ")
+    print(*answer) if type(answer) == list else print(answer)
